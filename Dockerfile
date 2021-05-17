2ee7e6c739348fae3cd400aa553759d8c1f51c30
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-05-17 12:26:23 +0300

    Pass secrets as secret files to docker image

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,7 @@ FROM hasura/graphql-engine:v1.3.3.cli-migrations-v2
 ENV HASURA_GRAPHQL_UNAUTHORIZED_ROLE="anonymous"
 COPY ./hasura/migrations /hasura-migrations
 COPY ./hasura/metadata /hasura-metadata
+WORKDIR /app
+COPY ./docker-entrypoint.sh ./
+ENTRYPOINT ["/app/docker-entrypoint.sh"]
+CMD ["graphql-engine", "serve"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -euo pipefail
+
+SECRET_STORE_BASE_PATH="${SECRET_STORE_BASE_PATH:-/run/secrets}"
+HASURA_ADMIN_SECRET="$(cat ${SECRET_STORE_BASE_PATH}/hasura-admin-secret)"
+DB_USERNAME="$(cat ${SECRET_STORE_BASE_PATH}/db-username)"
+DB_PASSWORD="$(cat ${SECRET_STORE_BASE_PATH}/db-password)"
+DB_HOSTNAME="$(cat ${SECRET_STORE_BASE_PATH}/db-hostname)"
+DB_NAME="$(cat ${SECRET_STORE_BASE_PATH}/db-name)"
+
+# This script extends the functionality of the original Hasura docker image’s /bin/docker-entrypoint.sh script
+# HASURA_GRAPHQL_DATABASE_URL format: postgres://<user>:<password>@<host>:<port>/<db-name>
+HASURA_GRAPHQL_ADMIN_SECRET="$HASURA_ADMIN_SECRET" \
+  HASURA_GRAPHQL_DATABASE_URL="postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_NAME}" \
+  exec /bin/docker-entrypoint.sh "$@"
