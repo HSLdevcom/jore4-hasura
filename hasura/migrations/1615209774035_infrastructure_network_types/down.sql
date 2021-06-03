2404101497090f65c2557dcf712a71e7d469eec1
Tamas Pallos <tamas.pallos@vincit.fi>
2021-06-03 13:55:25 +0300

    Remove old migrations

--- hasura/migrations/1615209774035_infrastructure_network_types/down.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS
-  infrastructure_network.infrastructure_network_types
-  CASCADE;
-
-ALTER TABLE IF EXISTS
-  infrastructure_network.infrastructure_links
-  DROP COLUMN IF EXISTS
-  infrastructure_network_type_id
-  CASCADE;
