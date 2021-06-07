c3e4c3096989d2f373ca1dfb30c1e3d9a29daa36
haphut <haphut@mistmap.com>
2021-06-07 07:54:42 +0300

    Replace placeholders in Docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,12 @@ DB_USERNAME="$(cat "${SECRET_STORE_BASE_PATH}/db-username")"
 DB_PASSWORD="$(cat "${SECRET_STORE_BASE_PATH}/db-password")"
 DB_HOSTNAME="$(cat "${SECRET_STORE_BASE_PATH}/db-hostname")"
 DB_NAME="$(cat "${SECRET_STORE_BASE_PATH}/db-name")"
+REPLACE_PLACEHOLDERS_SCRIPT='/app/replace-placeholders-in-sql-schema-migrations.sh'
+
+# Replace the possible placeholders in the SQL schema migrations.
+#
+# HASURA_GRAPHQL_MIGRATIONS_DIR is defined in the Dockerfile.
+"${REPLACE_PLACEHOLDERS_SCRIPT}" "${SECRET_STORE_BASE_PATH}" "${HASURA_GRAPHQL_MIGRATIONS_DIR}"
 
 # This script extends the functionality of the original Hasura docker image’s /bin/docker-entrypoint.sh script
 # HASURA_GRAPHQL_DATABASE_URL format: postgres://<user>:<password>@<host>:<port>/<db-name>
