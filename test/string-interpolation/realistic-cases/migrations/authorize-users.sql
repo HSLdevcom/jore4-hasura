e800064119d05ffe4322b6a02803b85f143aaaed
haphut <haphut@mistmap.com>
2021-06-21 10:20:55 +0300

    Add test for realistic SQL

---
+++ test/string-interpolation/realistic-cases/migrations/authorize-users.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA IF NOT EXISTS jore3 AUTHORIZATION xxx_hsl_jore4_db_jore3importer_username_xxx;
+CREATE SCHEMA IF NOT EXISTS internal_jore3 AUTHORIZATION xxx_hsl_jore4_db_jore3importer_username_xxx;
+
+GRANT
+  USAGE
+  ON SCHEMA jore3
+  TO
+    xxx_hsl_jore4_db_jore3transformer_username_xxx,
+    xxx_hsl_jore4_db_mapmatcher_username_xxx;
+
+GRANT
+  SELECT, REFERENCES
+  ON ALL TABLES
+  IN SCHEMA jore3
+  TO
+    xxx_hsl_jore4_db_jore3transformer_username_xxx,
+    xxx_hsl_jore4_db_mapmatcher_username_xxx;
+
+GRANT
+  SELECT
+  ON ALL SEQUENCES
+  IN SCHEMA jore3
+  TO
+    xxx_hsl_jore4_db_jore3transformer_username_xxx,
+    xxx_hsl_jore4_db_mapmatcher_username_xxx;
+
+GRANT
+  EXECUTE
+  ON ALL ROUTINES
+  IN SCHEMA jore3
+  TO
+    xxx_hsl_jore4_db_jore3transformer_username_xxx,
+    xxx_hsl_jore4_db_mapmatcher_username_xxx;
