e8891f751bdf77a17569fd60ba6ac552d6601ed5
Jörg Schummer <jorg.schummer@vincit.fi>
2021-08-27 12:18:09 +0300

    f

--- hasura/migrations/1629368997917_init-auth-schema/up.sql
+++ hasura/migrations/1629368997917_init-auth-schema/up.sql
@@ -1,2 +1,2 @@
 CREATE SCHEMA auth_session;
-GRANT ALL PRIVILEGES ON SCHEMA auth_session TO xxx_hsl_jore4_db_auth_username_xxx;
+GRANT ALL PRIVILEGES ON SCHEMA auth_session TO xxx_db_auth_username_xxx;
--- hasura/migrations/1629444699083_create-test-data/up.sql
+++ hasura/migrations/1629444699083_create-test-data/up.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA test;
-GRANT ALL PRIVILEGES ON SCHEMA test TO xxx_hsl_jore4_db_hasura_username_xxx;
+GRANT ALL PRIVILEGES ON SCHEMA test TO xxx_db_username_xxx;
 
 CREATE TABLE test.data (message VARCHAR(255));
 INSERT INTO test.data VALUES ('Hello'), ('World!');
