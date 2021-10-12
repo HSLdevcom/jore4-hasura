d961d04bb2493fed57a7e1363090fb5644bbcf93
Jörg Schummer <jorg.schummer@vincit.fi>
2021-10-12 21:41:33 +0300

    Adopt changes from deploy scripts into local init-db.sql

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -5,3 +5,17 @@
 -- These users are created from the jore4-deploy repository in cloud environments.
 CREATE USER jore4auth PASSWORD 'jore4auth';
 CREATE USER jore3importer PASSWORD 'jore3importer';
+CREATE USER jore4hasura PASSWORD 'jore4hasurapassword';
+
+-- create the extensions used
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- create the schema required by the hasura system
+CREATE SCHEMA IF NOT EXISTS hdb_catalog;
+
+-- make the user an owner of system schemas
+ALTER SCHEMA hdb_catalog OWNER TO jore4hasura;
+
+-- allow hasura to create new schemas in migrations
+GRANT CREATE ON DATABASE postgres TO jore4hasura;
