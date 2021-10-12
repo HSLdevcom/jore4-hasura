22be91eae73a56551ff548f890ec32ad42c0a415
Jörg Schummer <jorg.schummer@vincit.fi>
2021-10-12 21:26:21 +0300

    Change migrations to allow Hasura to run with non-superuser pg role
    - removed temporary schema cleanup migration used for rapid development
    - removed extension management
    - removed user authorization
    - changed jore3importer schema creation to not require superuser role
    
    This means that database users will need to be created and their
    priviliges will need to be configured using the deploy scripts in the
    future.

--- migrations/default/1620000000600_import_schemas/up.sql
+++ migrations/default/1620000000600_import_schemas/up.sql
@@ -1,1 +1,2 @@
-CREATE SCHEMA import_jore3 AUTHORIZATION xxx_db_jore3importer_username_xxx;
+CREATE SCHEMA import_jore3;
+GRANT ALL PRIVILEGES ON SCHEMA import_jore3 TO xxx_db_jore3importer_username_xxx;
