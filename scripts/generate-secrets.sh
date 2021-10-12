af40c17a094d3a78dfe3cb020681dbf11c356aee
Jörg Schummer <jorg.schummer@vincit.fi>
2021-10-12 21:40:59 +0300

    Make generate-secrets.sh use static secrets
    ...to ease testing and local development.

--- scripts/generate-secrets.sh
+++ scripts/generate-secrets.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# NB: In some environments (namely in the CI), /dev/urandom might not be available for entropy. Therefore,
-# secrets should be generated using another method and passed to this script as environment variables
-# (JORE4HASURA_HASURA_ADMIN_SECRET and JORE4HASURA_DB_PASSWORD, see below).
-
 set -Eeuo pipefail
 
 SECRETS_DIR='./secrets'
@@ -23,12 +19,6 @@ prompt () {
   done
 }
 
-generate_password () {
-  # SIGPIPE ensues from writing into the pipe after the reading stops so use
-  # echo for pipefail.
-  echo "$(LC_CTYPE=C </dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-}
-
 FORCE=
 
 while [[ $# -gt 0 ]]; do
@@ -46,26 +36,28 @@ done
 
 [ -z "$FORCE" ] && prompt
 
-HASURA_ADMIN_SECRET="${JORE4HASURA_HASURA_ADMIN_SECRET:-$(generate_password)}"
-DB_USERNAME='postgres'
-DB_PASSWORD="${JORE4HASURA_DB_PASSWORD:-$(generate_password)}"
+HASURA_ADMIN_SECRET='hasuraadminsecret'
 DB_HOSTNAME='postgis'
 DB_NAME='postgres'
+DB_ADMIN_USERNAME='postgres'
+DB_ADMIN_PASSWORD='postgrespassword'
+DB_HASURA_USERNAME='jore4hasura'
+DB_HASURA_PASSWORD='jore4hasurapassword'
 DB_AUTH_USERNAME='jore4auth'
 JORE3_IMPORTER_DB_USERNAME='jore3importer'
 
 # Write the secrets for Hasura.
 mkdir -p "${SECRETS_DIR}"
 echo "${HASURA_ADMIN_SECRET}" > "${SECRETS_DIR}/hasura-admin-secret"
-echo "${DB_USERNAME}" > "${SECRETS_DIR}/db-username"
-echo "${DB_PASSWORD}" > "${SECRETS_DIR}/db-password"
 echo "${DB_HOSTNAME}" > "${SECRETS_DIR}/db-hostname"
 echo "${DB_NAME}" > "${SECRETS_DIR}/db-name"
+echo "${DB_HASURA_USERNAME}" > "${SECRETS_DIR}/db-username"
+echo "${DB_HASURA_PASSWORD}" > "${SECRETS_DIR}/db-password"
 echo "${DB_AUTH_USERNAME}" > "${SECRETS_DIR}/db-auth-username"
 echo "${JORE3_IMPORTER_DB_USERNAME}" > "${SECRETS_DIR}/db-jore3importer-username"
 
 # Write the secrets in the .env file.
 : > "${ENV_PATH}"
-echo "POSTGRES_USER=${DB_USERNAME}" >> "${ENV_PATH}"
-echo "POSTGRES_PASSWORD=${DB_PASSWORD}" >> "${ENV_PATH}"
+echo "POSTGRES_USER=${DB_ADMIN_USERNAME}" >> "${ENV_PATH}"
+echo "POSTGRES_PASSWORD=${DB_ADMIN_PASSWORD}" >> "${ENV_PATH}"
 echo "HASURA_GRAPHQL_ADMIN_SECRET=${HASURA_ADMIN_SECRET}" >> "${ENV_PATH}"
