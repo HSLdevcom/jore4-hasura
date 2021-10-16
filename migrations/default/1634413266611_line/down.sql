b90becc983e3254bb80910db0b2fb446ffa167fb
Jörg Schummer <jorg.schummer@vincit.fi>
2021-10-16 23:16:37 +0300

    Public transport line
    The line definition, a subset of the Transmodel line definition.

---
+++ migrations/default/1634413266611_line/down.sql
@@ -0,0 +1,65 @@
+
+ALTER TABLE journey_pattern.journey_pattern
+  DROP COLUMN line_id;
+
+DROP TABLE journey_pattern.line;
+
+-- restore old versions of view and modification functions
+
+CREATE OR REPLACE VIEW route.route AS
+  SELECT
+    r.route_id,
+    r.description_i18n,
+    r.starts_from_scheduled_stop_point_id,
+    r.ends_at_scheduled_stop_point_id,
+    -- FIXME: clamp with start and end stops: join on scheduled stop point view, ST_LineSubstring, consider direction
+    ST_LineMerge(
+      ST_Collect(
+        CASE
+          WHEN ilar.is_traversal_forwards THEN il.shape::geometry
+          ELSE ST_Reverse(il.shape::geometry)
+          END
+        )
+      )::geography AS route_shape
+  FROM
+    internal_route.route AS r
+  LEFT JOIN (
+    route.infrastructure_link_along_route AS ilar
+      INNER JOIN infrastructure_network.infrastructure_link AS il
+      ON (ilar.infrastructure_link_id = il.infrastructure_link_id)
+    ) ON (r.route_id = ilar.route_id)
+  GROUP BY r.route_id;
+
+CREATE OR REPLACE FUNCTION route.insert_route ()
+  RETURNS TRIGGER
+  LANGUAGE plpgsql
+AS $route_insert_route$
+BEGIN
+  INSERT INTO internal_route.route (
+    description_i18n,
+    starts_from_scheduled_stop_point_id,
+    ends_at_scheduled_stop_point_id
+  ) VALUES (
+     NEW.description_i18n,
+     NEW.starts_from_scheduled_stop_point_id,
+     NEW.ends_at_scheduled_stop_point_id
+   ) RETURNING route_id INTO NEW.route_id;
+  RETURN NEW;
+END;
+$route_insert_route$;
+
+CREATE OR REPLACE FUNCTION route.update_route ()
+  RETURNS TRIGGER
+  LANGUAGE plpgsql
+AS $route_update_route$
+BEGIN
+  UPDATE internal_route.route
+  SET
+    route_id = NEW.route_id,
+    description_i18n = NEW.description_i18n,
+    starts_from_scheduled_stop_point_id = NEW.starts_from_scheduled_stop_point_id,
+    ends_at_scheduled_stop_point_id = NEW.ends_at_scheduled_stop_point_id
+  WHERE route_id = OLD.route_id;
+  RETURN NEW;
+END;
+$route_update_route$;
