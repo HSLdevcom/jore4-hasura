d28f1e8f02d9c383a8152179bd899e07af2eb506
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-11-22 11:54:07 +0200

    FIXUP to sql function definition
    Example query that returns link with direction:
    
    ```
    query MyQuery {
      infrastructure_network_find_closest_link_and_direction_aggregate(args: {point_of_interest: {type: "Point", coordinates: [24.939316517, 60.170781403]}, query_distance_in_meters: 10}) {
        nodes {
          closest_infrastructure_link_id
          point_distance_from_link
          traffic_flow_direction_of_point_on_link
        }
      }
    }
    ```
    
    (Same query with coordinates: [24.939316517, 60.170981403] returns "unknown" direction)

--- migrations/default/1637050842445_add_resolve_point_to_closest_link_and_direction_function/up.sql
+++ migrations/default/1637050842445_add_resolve_point_to_closest_link_and_direction_function/up.sql
@@ -1,33 +1,43 @@
 CREATE TABLE infrastructure_network.link_properties (
-    infrastructure_link_id uuid,
-    point_distance_from_link double precision,
-    traffic_flow_direction_of_point_on_link text
+  point_of_interest_geojson text,
+  closest_infrastructure_link_id uuid,
+  closest_infrastructure_link_shape_geojson text,
+  point_distance_from_link double precision,
+  traffic_flow_direction_of_point_on_link text
 );
 COMMENT ON TABLE
   infrastructure_network.link_properties IS
   'Helper table used by infrastructure_network.find_closest_link_and_direction function.
    Hasura requires custom sql functions to return data as table or SETOF table: https://hasura.io/docs/latest/graphql/core/databases/postgres/schema/custom-functions.html#supported-sql-functions';
 
-CREATE FUNCTION infrastructure_network.find_closest_link_and_direction(
-    geog geography,
-    radius_in_meters double precision
+CREATE FUNCTION infrastructure_network.find_closest_link_and_direction (
+    point_of_interest geography,
+    query_distance_in_meters double precision
+    -- OUT point_of_interest_geojson text,
+    -- OUT closest_infrastructure_link_id uuid,
+    -- OUT closest_infrastructure_link_shape_geojson text,
+    -- OUT point_distance_from_link double precision,
+    -- OUT traffic_flow_direction_of_point_on_link text
 )
+-- RETURNS RECORD
 RETURNS SETOF infrastructure_network.link_properties
-LANGUAGE sql
-STABLE
-STRICT
-PARALLEL SAFE
+LANGUAGE SQL
+    STABLE
+    STRICT
+    PARALLEL SAFE
 AS $function$
 SELECT
+    ST_AsGeoJSON(point_of_interest),
     link.infrastructure_link_id,
-    closest_link_result.distance AS point_distance_from_link,
-    direction.direction AS traffic_flow_direction_of_point_on_link
+    ST_AsGeoJSON(link.shape),
+    closest_link_result.distance,
+    direction.direction
 FROM (
     SELECT
         infrastructure_link_id,
-        geog <-> shape AS distance
+        point_of_interest <-> shape AS distance
     FROM infrastructure_network.infrastructure_link
-    WHERE ST_DWithin(geog, shape, radius_in_meters)
+    WHERE ST_DWithin(point_of_interest, shape, query_distance_in_meters)
     ORDER BY distance
     LIMIT 1
 ) closest_link_result
@@ -35,38 +45,20 @@ INNER JOIN infrastructure_network.infrastructure_link link
     ON link.infrastructure_link_id = closest_link_result.infrastructure_link_id
 CROSS JOIN LATERAL (
     SELECT
-        ST_Transform(ST_Force2D(geog::geometry), srid) AS point_geom,
+        ST_Transform(ST_Force2D(point_of_interest::geometry), srid) AS point_geom,
         ST_Transform(ST_Force2D(link.shape::geometry), srid) AS link_geom
     FROM (
-        SELECT internal_utils.determine_srid(link.shape, geog) AS srid
+        SELECT internal_utils.determine_srid(link.shape, point_of_interest) AS srid
     ) best
 ) geom_2d
 CROSS JOIN LATERAL (
     SELECT
-        ST_Intersection(
-            round_buffer_geom,
-            ST_Difference(lhs_offset_buffer_geom, rhs_side_buffer_geom)
-        ) AS lhs_geom,
-        ST_Intersection(
-            round_buffer_geom,
-            ST_Difference(rhs_offset_buffer_geom, lhs_side_buffer_geom)
-        ) AS rhs_geom
+        ST_Buffer(link_geom, capped_radius, 'side=left' ) AS lhs_side_buf_geom,
+        ST_Buffer(link_geom, capped_radius, 'side=right') AS rhs_side_buf_geom
     FROM (
-        SELECT
-            ST_Buffer(geom_2d.link_geom, radius_in_meters, 'join=round') AS round_buffer_geom,
-
-            ST_Buffer(geom_2d.link_geom, radius_in_meters, 'side=left' ) AS lhs_side_buffer_geom,
-            ST_Buffer(geom_2d.link_geom, radius_in_meters, 'side=right') AS rhs_side_buffer_geom,
-
-            ST_Buffer(lhs_offset_line_geom, radius_in_meters, 'endcap=square join=mitre') AS lhs_offset_buffer_geom,
-            ST_Buffer(rhs_offset_line_geom, radius_in_meters, 'endcap=square join=mitre') AS rhs_offset_buffer_geom
-        FROM (
-            SELECT
-                ST_OffsetCurve(geom_2d.link_geom,  radius_in_meters, 'quad_segs=8 join=round') AS lhs_offset_line_geom,
-                ST_OffsetCurve(geom_2d.link_geom, -radius_in_meters, 'quad_segs=8 join=round') AS rhs_offset_line_geom
-        ) offset_line
-    ) buf
-) split_round_buffer
+        SELECT least(query_distance_in_meters, floor(ST_Length(link_geom))) AS capped_radius
+    ) buf_radius
+) side_buf
 CROSS JOIN LATERAL (
     SELECT
         CASE
@@ -76,8 +68,8 @@ CROSS JOIN LATERAL (
         END AS direction
     FROM (
         SELECT
-            ST_Contains(split_round_buffer.lhs_geom, geom_2d.point_geom) AS in_left,
-            ST_Contains(split_round_buffer.rhs_geom, geom_2d.point_geom) AS in_right
+            ST_Contains(lhs_side_buf_geom, point_geom) AS in_left,
+            ST_Contains(rhs_side_buf_geom, point_geom) AS in_right
     ) direction_test
 ) direction;
 $function$;
