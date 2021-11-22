7244ca14382514328f651814f66dd511912c318c
Jörg Schummer <jorg.schummer@vincit.fi>
2021-11-22 02:09:42 +0200

    Add unique identifiers for lines and routes
    A route is uniquely identified by its label and direction (for a given
    priority and validity period).
    
    A line is uniquely identified by its label (for a given priority and
    validity period).
    
    This commit adds these columns to the database schema.

---
+++ migrations/default/1637329168554_unique_identifiers_for_lines_and_routes/down.sql
@@ -0,0 +1,42 @@
+
+-- line changes
+----------------
+
+ALTER TABLE route.line
+  DROP COLUMN label;
+
+-- route changes
+-----------------
+
+DROP VIEW route.route;
+
+ALTER VIEW deleted.route_1637329168554
+SET SCHEMA route;
+
+ALTER VIEW route.route_1637329168554
+RENAME TO route;
+
+ALTER TABLE internal_route.route
+DROP COLUMN label,
+DROP COLUMN direction;
+
+DROP FUNCTION route.insert_route;
+
+ALTER FUNCTION deleted.insert_route_1637329168554 ()
+SET SCHEMA route;
+
+ALTER FUNCTION route.insert_route_1637329168554 ()
+RENAME TO insert_route;
+
+DROP FUNCTION route.update_route;
+
+ALTER FUNCTION deleted.update_route_1637329168554 ()
+  SET SCHEMA route;
+
+ALTER FUNCTION route.update_route_1637329168554 ()
+  RENAME TO update_route;
+
+-- general
+-----------
+
+DROP SCHEMA deleted;
