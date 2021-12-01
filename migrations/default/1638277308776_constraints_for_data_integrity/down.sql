e64c1ec0e35ae26483c56ee560a1319ad818c4e8
Jörg Schummer <jorg.schummer@vincit.fi>
2021-12-01 23:35:18 +0200

    Additional line <-> route data integrity constraints
    - every route has to belong to a line
    - routes must not have a lower priority than their corresponding line
    - change Hasura's transaction isolation level to 'serializable' to be
      able to implement reliable constraint triggers

---
+++ migrations/default/1638277308776_constraints_for_data_integrity/down.sql
@@ -0,0 +1,15 @@
+
+DROP TRIGGER check_line_routes_priorities_trigger
+  ON route.line;
+
+DROP FUNCTION route.check_line_routes_priorities();
+
+
+DROP TRIGGER check_route_line_priorities_trigger
+  ON internal_route.route;
+
+DROP FUNCTION internal_route.check_route_line_priorities();
+
+
+ALTER TABLE internal_route.route
+ALTER COLUMN on_line_id DROP NOT NULL;
