57fe12be374a9d34ffee58ab9dc9371e0d546179
Jörg Schummer <jorg.schummer@vincit.fi>
2021-12-28 22:49:41 +0200

    Set CASCADE on infra link and stop point vehicle_(sub)mode refs
    The information contained by the vehicle_submode_on_infrastructure_link
    and vehicle_mode_on_scheduled_stop_point tables should be interpreted to
    hold information on the infrastructure_link and scheduled_stop_point
    entities, respectively.
    
    For this reason, it is safe to cascade those tables' affected rows'
    information when the entitity referencing those rows is changed such
    that the reference is broken.

---
+++ migrations/default/1640721052260_set_cascade_on_fk_service_pattern_vehicle_mode_on_scheduled_stop_point_scheduled_stop_point_id/down.sql
@@ -0,0 +1,7 @@
+ALTER TABLE service_pattern.vehicle_mode_on_scheduled_stop_point
+  DROP CONSTRAINT vehicle_mode_on_scheduled_stop_point_scheduled_stop_point_id_fkey,
+  ADD CONSTRAINT scheduled_stop_point_serviced_by_v_scheduled_stop_point_id_fkey
+    FOREIGN KEY (scheduled_stop_point_id)
+  REFERENCES internal_service_pattern.scheduled_stop_point (scheduled_stop_point_id)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
