a38ba9bcb8ea0b29917fa00db2f5dd7566782764
Jörg Schummer <jorg.schummer@vincit.fi>
2021-12-31 21:19:33 +0200

    Set CASCADE on scheduled_stop_point_in_journey_pattern foreign key
    ...from scheduled_stop_point and journey_pattern.

---
+++ migrations/default/1640941173820_set_fk_journey_pattern_scheduled_stop_point_in_journey_pattern_scheduled_stop_point_id/up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE journey_pattern.scheduled_stop_point_in_journey_pattern
+  DROP CONSTRAINT scheduled_stop_point_in_journey_pa_scheduled_stop_point_id_fkey,
+  ADD CONSTRAINT scheduled_stop_point_in_journey_pattern_scheduled_stop_point_id_fkey
+    FOREIGN KEY (scheduled_stop_point_id)
+      REFERENCES internal_service_pattern.scheduled_stop_point (scheduled_stop_point_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE;
