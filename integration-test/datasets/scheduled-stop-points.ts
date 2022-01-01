2cf4dde4161e7065e14169e98377fba80c6e45ca
Jörg Schummer <jorg.schummer@vincit.fi>
2022-01-01 21:37:10 +0200

    Fix existing tests to work with the stop constraints

--- integration-test/datasets/scheduled-stop-points.ts
+++ integration-test/datasets/scheduled-stop-points.ts
@@ -61,6 +61,78 @@ export const scheduledStopPoints: ScheduledStopPoint[] = [
     validity_start: null,
     validity_end: new Date("2064-01-03 12:34:56"),
   },
+  {
+    scheduled_stop_point_id: "f150e375-ebe0-448b-a974-cdc2f482268d",
+    located_on_infrastructure_link_id:
+      infrastructureLinks[8].infrastructure_link_id,
+    direction: LinkDirection.Forward,
+    measured_location: {
+      type: "Point",
+      coordinates: [12.1, 11.2, 0],
+      crs: {
+        properties: { name: "urn:ogc:def:crs:EPSG::4326" },
+        type: "name",
+      },
+    },
+    label: "stopX",
+    priority: 10,
+    validity_start: new Date("2065-01-01 12:34:56"),
+    validity_end: new Date("2065-02-01 12:34:56"),
+  },
+  {
+    scheduled_stop_point_id: "02eca2f4-743a-4505-99ed-52390970a3d2",
+    located_on_infrastructure_link_id:
+      infrastructureLinks[9].infrastructure_link_id,
+    direction: LinkDirection.Backward,
+    measured_location: {
+      type: "Point",
+      coordinates: [8.1, 7.2, 0],
+      crs: {
+        properties: { name: "urn:ogc:def:crs:EPSG::4326" },
+        type: "name",
+      },
+    },
+    label: "stopY",
+    priority: 10,
+    validity_start: new Date("2065-02-03 12:34:56"),
+    validity_end: null,
+  },
+  {
+    scheduled_stop_point_id: "8ef30378-1e3b-4646-aa67-e7ee8051690e",
+    located_on_infrastructure_link_id:
+      infrastructureLinks[4].infrastructure_link_id,
+    direction: LinkDirection.Forward,
+    measured_location: {
+      type: "Point",
+      coordinates: [12.1, 11.2, 0],
+      crs: {
+        properties: { name: "urn:ogc:def:crs:EPSG::4326" },
+        type: "name",
+      },
+    },
+    label: "stopZ",
+    priority: 10,
+    validity_start: new Date("2065-01-01 12:34:56"),
+    validity_end: new Date("2065-02-01 12:34:56"),
+  },
+  {
+    scheduled_stop_point_id: "a1b6e453-07b1-4c31-8ade-6872994bc857",
+    located_on_infrastructure_link_id:
+      infrastructureLinks[5].infrastructure_link_id,
+    direction: LinkDirection.Forward,
+    measured_location: {
+      type: "Point",
+      coordinates: [10.1, 9.2, 0],
+      crs: {
+        properties: { name: "urn:ogc:def:crs:EPSG::4326" },
+        type: "name",
+      },
+    },
+    label: "stopZ2",
+    priority: 30,
+    validity_start: null,
+    validity_end: new Date("2064-01-03 12:34:56"),
+  },
 ];
 
 export const vehicleModeOnScheduledStopPoint: VehicleModeOnScheduledStopPoint[] =
@@ -79,6 +151,26 @@ export const vehicleModeOnScheduledStopPoint: VehicleModeOnScheduledStopPoint[] 
     },
     {
       scheduled_stop_point_id: scheduledStopPoints[2].scheduled_stop_point_id,
+      vehicle_mode: VehicleMode.Bus,
+    },
+    {
+      scheduled_stop_point_id: scheduledStopPoints[3].scheduled_stop_point_id,
+      vehicle_mode: VehicleMode.Bus,
+    },
+    {
+      scheduled_stop_point_id: scheduledStopPoints[4].scheduled_stop_point_id,
+      vehicle_mode: VehicleMode.Bus,
+    },
+    {
+      scheduled_stop_point_id: scheduledStopPoints[5].scheduled_stop_point_id,
+      vehicle_mode: VehicleMode.Bus,
+    },
+    {
+      scheduled_stop_point_id: scheduledStopPoints[5].scheduled_stop_point_id,
+      vehicle_mode: VehicleMode.Tram,
+    },
+    {
+      scheduled_stop_point_id: scheduledStopPoints[6].scheduled_stop_point_id,
       vehicle_mode: VehicleMode.Bus,
     },
   ];
