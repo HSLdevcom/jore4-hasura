722679bbdd49e25ebd495172b44f78d6eae78d31
Jörg Schummer <jorg.schummer@vincit.fi>
2022-01-24 18:28:28 +0200

    Integration tests: Added test with complex route (116 inbound)

---
+++ integration-test/datasets/route116/lines.ts
@@ -0,0 +1,15 @@
+import { Line, VehicleMode } from "@datasets/types";
+
+export const lines: Line[] = [
+  {
+    line_id: "eb12d002-b4ad-4071-b2eb-2291692d37d2",
+    name_i18n: "Valkjärventie - Tapiola",
+    short_name_i18n: null,
+    description_i18n: null,
+    primary_vehicle_mode: VehicleMode.Bus,
+    label: "116",
+    priority: 10,
+    validity_start: null,
+    validity_end: null,
+  },
+];
---
+++ integration-test/tests/data-integrity/route-journey-pattern-refs/insert-complicated-route.spec.ts
@@ -0,0 +1,75 @@
+import * as pg from "pg";
+import * as config from "@config";
+import "@util/matchers";
+import { queryTable, setupDb } from "@datasets/setup";
+import { route116TableConfig } from "@datasets/route116";
+import {
+  journeyPatterns,
+  scheduledStopPointInJourneyPattern,
+} from "@datasets/route116/journey-patterns";
+import {
+  infrastructureLinkAlongRoute,
+  routes,
+} from "@datasets/route116/routes";
+
+describe("Inserting a complicated route", () => {
+  let dbConnectionPool: pg.Pool;
+
+  beforeAll(() => {
+    dbConnectionPool = new pg.Pool(config.databaseConfig);
+  });
+
+  afterAll(() => dbConnectionPool.end());
+
+  beforeEach(() => setupDb(dbConnectionPool, route116TableConfig));
+
+  it("should create the route correctly in the database", async () => {
+    const routeResponse = await queryTable(
+      dbConnectionPool,
+      "route.route",
+      route116TableConfig
+    );
+
+    expect(routeResponse.rowCount).toEqual(routes.length);
+    expect(routeResponse.rows).toEqual(expect.arrayContaining(routes));
+
+    const infrastructureLinkAlongRouteResponse = await queryTable(
+      dbConnectionPool,
+      "route.infrastructure_link_along_route",
+      route116TableConfig
+    );
+
+    expect(infrastructureLinkAlongRouteResponse.rowCount).toEqual(
+      infrastructureLinkAlongRoute.length
+    );
+    expect(infrastructureLinkAlongRouteResponse.rows).toEqual(
+      expect.arrayContaining(infrastructureLinkAlongRoute)
+    );
+  });
+
+  it("should create the journey pattern correctly in the database", async () => {
+    const journeyPatternResponse = await queryTable(
+      dbConnectionPool,
+      "journey_pattern.journey_pattern",
+      route116TableConfig
+    );
+
+    expect(journeyPatternResponse.rowCount).toEqual(journeyPatterns.length);
+    expect(journeyPatternResponse.rows).toEqual(
+      expect.arrayContaining(journeyPatterns)
+    );
+
+    const scheduledStopPointInJourneyPatternResponse = await queryTable(
+      dbConnectionPool,
+      "journey_pattern.scheduled_stop_point_in_journey_pattern",
+      route116TableConfig
+    );
+
+    expect(scheduledStopPointInJourneyPatternResponse.rowCount).toEqual(
+      scheduledStopPointInJourneyPattern.length
+    );
+    expect(scheduledStopPointInJourneyPatternResponse.rows).toEqual(
+      expect.arrayContaining(scheduledStopPointInJourneyPattern)
+    );
+  });
+});
