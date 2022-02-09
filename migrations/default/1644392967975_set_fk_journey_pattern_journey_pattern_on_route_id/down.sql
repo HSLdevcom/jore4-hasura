68f482dbfca1473b1a76f08cf3e92190439c4c2d
Eetu Niemi <eetu.niemi@vincit.fi>
2022-02-09 14:38:59 +0200

    Delete journey pattern when its route is deleted
    - Set journey pattern's foreign key referencing route to cascade on delete

---
+++ migrations/default/1644392967975_set_fk_journey_pattern_journey_pattern_on_route_id/down.sql
@@ -0,0 +1,5 @@
+alter table "journey_pattern"."journey_pattern" drop constraint "journey_pattern_on_route_id_fkey",
+  add constraint "journey_pattern_on_route_id_fkey"
+  foreign key ("on_route_id")
+  references "internal_route"."route"
+  ("route_id") on update no action on delete no action;
