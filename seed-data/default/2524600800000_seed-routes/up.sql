b16e27246720ba96874eddb50042bb439d7d7ebf
Tamas Pallos <tamas.pallos@vincit.fi>
2022-03-07 12:24:21 +0200

    Remove description field from seed lines

--- seed-data/default/2524600800000_seed-routes/up.sql
+++ seed-data/default/2524600800000_seed-routes/up.sql
@@ -2,12 +2,12 @@
 -- warning: this data does not actually make any sense, should update it when we get real data from jore3
 
 INSERT INTO route.line
-  (line_id,name_i18n,short_name_i18n,description_i18n,primary_vehicle_mode,label,priority,validity_start,validity_end)
+  (line_id,name_i18n,short_name_i18n,primary_vehicle_mode,label,priority,validity_start,validity_end)
 VALUES
-  ('101f800c-39ed-4d85-8ece-187cd9fe1c5e','Rautatientori - Veräjälaakso','Rautatientori - Veräjälaakso','Rautatientori - Kätilöopisto - Veräjälaakso','bus','65',10,'2021-01-01','2023-12-13'),
-  ('9058c328-efdd-412c-9b2b-37b0f6a2c6fb','Rautatientori - Malmi as.','Rautatientori - Malmi as.','Rautatientori - Arabia - Malmi as.','bus','71',10,'2021-01-01','2023-12-13'),
-  ('bbd1cb29-74c3-4fa1-ac86-918d7fa96fe2','Rautatientori - Nikkilä','Rautatientori - Nikkilä','Rautatientori - Nikkilä','bus','785K',10,'2021-01-01','2023-12-13'),
-  ('db748c5c-42e3-429f-baa0-e0db227dc2c8','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','bus','1234',10,'2021-01-01','2023-12-13')
+  ('101f800c-39ed-4d85-8ece-187cd9fe1c5e','Rautatientori - Veräjälaakso','Rautatientori - Veräjälaakso','bus','65',10,'2021-01-01','2023-12-13'),
+  ('9058c328-efdd-412c-9b2b-37b0f6a2c6fb','Rautatientori - Malmi as.','Rautatientori - Malmi as.','bus','71',10,'2021-01-01','2023-12-13'),
+  ('bbd1cb29-74c3-4fa1-ac86-918d7fa96fe2','Rautatientori - Nikkilä','Rautatientori - Nikkilä','bus','785K',10,'2021-01-01','2023-12-13'),
+  ('db748c5c-42e3-429f-baa0-e0db227dc2c8','Erottaja - Arkkadiankatu','Erottaja - Arkkadiankatu','bus','1234',10,'2021-01-01','2023-12-13')
 ON CONFLICT DO NOTHING;
 
 -- to avoid constraint conflicts, inserting scheduled_stop_points and their allowed vehicle modes at the same time, within a transaction
