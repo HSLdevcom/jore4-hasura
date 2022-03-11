d6131f9b11d70469e34e39784bbac4dde2ba73cc
Jörg Schummer <jorg.schummer@vincit.fi>
2022-03-11 09:54:31 +0200

    Grant missing DB permissions to jore3-importer user

---
+++ migrations/default/1646743091995_grant_missing_permissions_to_importer_user/down.sql
@@ -0,0 +1,13 @@
+
+REVOKE USAGE ON SCHEMA infrastructure_network FROM xxx_db_jore3importer_username_xxx;
+REVOKE SELECT ON ALL TABLES IN SCHEMA infrastructure_network FROM xxx_db_jore3importer_username_xxx;
+
+REVOKE USAGE ON SCHEMA reusable_components FROM xxx_db_jore3importer_username_xxx;
+REVOKE SELECT ON ALL TABLES IN SCHEMA reusable_components FROM xxx_db_jore3importer_username_xxx;
+
+REVOKE USAGE ON SCHEMA service_pattern FROM xxx_db_jore3importer_username_xxx;
+REVOKE USAGE ON SCHEMA journey_pattern FROM xxx_db_jore3importer_username_xxx;
+REVOKE USAGE ON SCHEMA internal_service_pattern FROM xxx_db_jore3importer_username_xxx;
+REVOKE USAGE ON SCHEMA internal_route FROM xxx_db_jore3importer_username_xxx;
+REVOKE USAGE ON SCHEMA route FROM xxx_db_jore3importer_username_xxx;
+REVOKE USAGE ON SCHEMA internal_utils FROM xxx_db_jore3importer_username_xxx;
---
+++ migrations/default/1646743091995_grant_missing_permissions_to_importer_user/up.sql
@@ -0,0 +1,13 @@
+
+GRANT USAGE ON SCHEMA infrastructure_network TO xxx_db_jore3importer_username_xxx;
+GRANT SELECT ON ALL TABLES IN SCHEMA infrastructure_network TO xxx_db_jore3importer_username_xxx;
+
+GRANT USAGE ON SCHEMA reusable_components TO xxx_db_jore3importer_username_xxx;
+GRANT SELECT ON ALL TABLES IN SCHEMA reusable_components TO xxx_db_jore3importer_username_xxx;
+
+GRANT USAGE ON SCHEMA service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT USAGE ON SCHEMA journey_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT USAGE ON SCHEMA internal_service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT USAGE ON SCHEMA internal_route TO xxx_db_jore3importer_username_xxx;
+GRANT USAGE ON SCHEMA route TO xxx_db_jore3importer_username_xxx;
+GRANT USAGE ON SCHEMA internal_utils TO xxx_db_jore3importer_username_xxx;
