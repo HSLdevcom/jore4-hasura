589f723ff60783f1ecaa91a2893e03335ebfbccb
Jörg Schummer <jorg.schummer@vincit.fi>
2022-05-11 15:51:57 +0300

    Adopt existing integration tests for stop point label referencing

--- integration-test/tests/data-integrity/stop-point-infra-link-direction/update-infra-link.spec.ts
+++ integration-test/tests/data-integrity/stop-point-infra-link-direction/update-infra-link.spec.ts
@@ -50,6 +50,7 @@ describe('Update infrastructure link', () => {
       getTableConfigArray([
         'infrastructure_network.infrastructure_link',
         'infrastructure_network.vehicle_submode_on_infrastructure_link',
+        'internal_service_pattern.scheduled_stop_point_invariant',
         'internal_service_pattern.scheduled_stop_point',
         'service_pattern.vehicle_mode_on_scheduled_stop_point',
         'route.line',
