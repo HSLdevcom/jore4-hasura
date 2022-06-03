73dfcb8292a0eb96724a1ddc486dfba3a020f606
Tamas Pallos <tamas.pallos@vincit.fi>
2022-06-03 14:48:38 +0300

    Fix type of line migration to work with data
    Create the new column without constraints first, filling it up with data, then adding the constraints

--- migrations/generic/default/1648639435595_type_of_line/up.sql
+++ migrations/generic/default/1648639435595_type_of_line/up.sql
@@ -27,12 +27,26 @@ CREATE INDEX ON
   route.type_of_line
   (belonging_to_vehicle_mode);
 
+-- create the column without constraints first
 ALTER TABLE route.line
-  ADD COLUMN type_of_line text NOT NULL REFERENCES route.type_of_line (type_of_line);
-
+  ADD COLUMN type_of_line text NULL;
 COMMENT ON COLUMN
   route.line.type_of_line IS
   'The type of the line.';
+
+-- Filling up the column with some initial content with the USING expression, then adding the constraints
+-- NOTE: cannot use the UPDATE statement here, as it fails with "cannot ALTER TABLE because it has pending trigger events" error
+ALTER TABLE route.line
+  ALTER COLUMN type_of_line SET DATA TYPE text USING
+    CASE
+      WHEN primary_vehicle_mode = 'train' THEN 'regional_rail_service'
+      WHEN primary_vehicle_mode = 'metro' THEN 'metro_service'
+      WHEN primary_vehicle_mode = 'bus' THEN 'regional_bus_service'
+      WHEN primary_vehicle_mode = 'tram' THEN 'regional_tram_service'
+      WHEN primary_vehicle_mode = 'ferry' THEN 'ferry_service'
+    END,
+  ALTER COLUMN type_of_line SET NOT NULL,
+  ADD CONSTRAINT line_type_of_line_fkey FOREIGN KEY (type_of_line) REFERENCES route.type_of_line (type_of_line);
 
 CREATE FUNCTION route.check_type_of_line_vehicle_mode()
   RETURNS trigger
