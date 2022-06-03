ef665443087887f0883c45e4aee349546b6e5da7
Tamas Pallos <tamas.pallos@vincit.fi>
2022-06-03 15:22:41 +0300

    Fix seed migrations so that they can be rerun without failure

--- migrations/seed-data/default/2524600700000_seed-infra-vehicle-submode/up.sql
+++ migrations/seed-data/default/2524600700000_seed-infra-vehicle-submode/up.sql
@@ -4,4 +4,5 @@ INSERT INTO infrastructure_network.vehicle_submode_on_infrastructure_link
 SELECT
   il.infrastructure_link_id,
   'generic_bus' AS vehicle_submode
-FROM infrastructure_network.infrastructure_link il;
+FROM infrastructure_network.infrastructure_link il
+ON CONFLICT DO NOTHING;
