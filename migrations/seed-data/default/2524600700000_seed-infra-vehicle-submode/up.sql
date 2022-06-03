417eaaa86e1de0768cb3d93094159a2c1502b78f
Tamas Pallos <tamas.pallos@vincit.fi>
2022-06-03 15:22:41 +0300

    Fix seed migrations so that they can be rerun without failure

--- migrations/seed-data/default/2524600700000_seed-infra-vehicle-submode/up.sql
+++ migrations/seed-data/default/2524600700000_seed-infra-vehicle-submode/up.sql
@@ -1,7 +1,13 @@
+-- For testing purposes when checking the migrations' data compatibility, we do not want delete the
+-- seed data in the "down" migrations... Which results in having some data in the database when
+-- testing the migrations.
+-- However when running the complementing "up" migrations again, these inserts get in conflict with
+-- the existing data, so we need to skip these errors using "ON CONFLICT DO NOTHING";
 
 -- just set all the links' vehicle submodes to "generic_bus"
 INSERT INTO infrastructure_network.vehicle_submode_on_infrastructure_link
 SELECT
   il.infrastructure_link_id,
   'generic_bus' AS vehicle_submode
-FROM infrastructure_network.infrastructure_link il;
+FROM infrastructure_network.infrastructure_link il
+ON CONFLICT DO NOTHING;
