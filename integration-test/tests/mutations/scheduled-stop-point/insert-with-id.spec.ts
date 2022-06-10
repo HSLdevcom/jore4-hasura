093c96119884ecd12ebf7ba824370f3b3773ada8
Jörg Schummer <jorg.schummer@vincit.fi>
2022-06-10 15:22:23 +0300

    New version of internal_service_pattern.insert_scheduled_stop_point
    ...for the Jore3 importer or other internal use. The old version inserted a row directly into the
    internal_service_pattern.scheduled_stop_point table, skipping any functionality implemented in the
    service_pattern.scheduled_stop_point view's INSTEAD OF INSERT trigger. The new version inserts the row
    into the view, making use of any functionality implemented in the INSTEAD OF INSERT trigger.

---
+++ integration-test/tests/mutations/scheduled-stop-point/insert-with-id.spec.ts
@@ -0,0 +1,139 @@
+import * as rp from 'request-promise';
+import * as pg from 'pg';
+import * as config from '@config';
+import * as dataset from '@util/dataset';
+import { infrastructureLinks } from '@datasets/defaultSetup/infrastructure-links';
+import {
+  scheduledStopPointInvariants,
+  scheduledStopPoints,
+} from '@datasets/defaultSetup/scheduled-stop-points';
+import {
+  LinkDirection,
+  ScheduledStopPoint,
+  ScheduledStopPointProps,
+  VehicleMode,
+} from '@datasets/types';
+import '@util/matchers';
+import { getPropNameArray, queryTable, setupDb } from '@datasets/setup';
+
+const toBeInserted: Partial<ScheduledStopPoint> = {
+  scheduled_stop_point_id: 'd1e8878f-da19-474a-b156-13ac41175789',
+  located_on_infrastructure_link_id:
+    infrastructureLinks[2].infrastructure_link_id,
+  direction: LinkDirection.Backward,
+  measured_location: {
+    type: 'Point',
+    coordinates: [12.3, 23.4, 34.5],
+    crs: {
+      properties: { name: 'urn:ogc:def:crs:EPSG::4326' },
+      type: 'name',
+    },
+  } as dataset.GeometryObject,
+  label: 'inserted stop point',
+  priority: 50,
+  validity_end: new Date('2060-11-04 15:30:40Z'),
+};
+
+const insertedDefaultValues: Partial<ScheduledStopPoint> = {
+  validity_start: null,
+};
+
+const VEHICLE_MODE = VehicleMode.Bus;
+
+const mutation = `
+  mutation {
+    insert_service_pattern_scheduled_stop_point(objects: ${dataset.toGraphQlObject(
+      {
+        ...toBeInserted,
+        vehicle_mode_on_scheduled_stop_point: {
+          data: {
+            vehicle_mode: VEHICLE_MODE,
+          },
+        },
+      },
+      ['direction', 'vehicle_mode'],
+    )}) {
+      returning {
+        ${getPropNameArray(ScheduledStopPointProps).join(',')}
+      }
+    }
+  }
+`;
+
+describe('Insert scheduled_stop_point', () => {
+  let dbConnectionPool: pg.Pool;
+
+  beforeAll(() => {
+    dbConnectionPool = new pg.Pool(config.databaseConfig);
+  });
+
+  afterAll(() => dbConnectionPool.end());
+
+  beforeEach(() => setupDb(dbConnectionPool));
+
+  it('should return correct response', async () => {
+    const response = await rp.post({
+      ...config.hasuraRequestTemplate,
+      body: { query: mutation },
+    });
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        data: {
+          insert_service_pattern_scheduled_stop_point: {
+            returning: [
+              {
+                ...dataset.asGraphQlTimestampObject(toBeInserted),
+                ...insertedDefaultValues,
+              },
+            ],
+          },
+        },
+      }),
+    );
+  });
+
+  it('should insert correct row into the database', async () => {
+    await rp.post({
+      ...config.hasuraRequestTemplate,
+      body: { query: mutation },
+    });
+
+    const response = await queryTable(
+      dbConnectionPool,
+      'service_pattern.scheduled_stop_point',
+    );
+
+    expect(response.rowCount).toEqual(scheduledStopPoints.length + 1);
+
+    expect(response.rows).toEqual(
+      expect.arrayContaining(
+        dataset.asDbGeometryObjectArray(
+          [
+            {
+              ...toBeInserted,
+              ...insertedDefaultValues,
+            },
+            ...scheduledStopPoints,
+          ],
+          ['measured_location'],
+        ),
+      ),
+    );
+
+    const stopPointInvariantResponse = await queryTable(
+      dbConnectionPool,
+      'internal_service_pattern.scheduled_stop_point_invariant',
+    );
+
+    expect(stopPointInvariantResponse.rowCount).toEqual(
+      scheduledStopPointInvariants.length + 1,
+    );
+    expect(stopPointInvariantResponse.rows).toEqual(
+      expect.arrayContaining([
+        { label: toBeInserted.label },
+        ...scheduledStopPointInvariants,
+      ]),
+    );
+  });
+});
---
+++ migrations/generic/default/1654783185212_update_importer_insert_scheduled_stop_point_function/down.sql
@@ -0,0 +1,54 @@
+
+DROP FUNCTION internal_service_pattern.insert_scheduled_stop_point_with_vehicle_mode(
+  scheduled_stop_point_id uuid,
+  measured_location geography(PointZ,4326),
+  located_on_infrastructure_link_id uuid,
+  direction text,
+  label text,
+  validity_start timestamp with time zone,
+  validity_end timestamp with time zone,
+  priority integer,
+  supported_vehicle_mode text
+);
+
+ALTER FUNCTION deleted.insert_scheduled_stop_point_1654783185212(
+  scheduled_stop_point_id uuid,
+  measured_location geography(PointZ,4326),
+  located_on_infrastructure_link_id uuid,
+  direction text,
+  label text,
+  validity_start timestamp with time zone,
+  validity_end timestamp with time zone,
+  priority integer,
+  supported_vehicle_mode text
+  )
+SET SCHEMA internal_service_pattern;
+
+ALTER FUNCTION internal_service_pattern.insert_scheduled_stop_point_1654783185212(
+  scheduled_stop_point_id uuid,
+  measured_location geography(PointZ,4326),
+  located_on_infrastructure_link_id uuid,
+  direction text,
+  label text,
+  validity_start timestamp with time zone,
+  validity_end timestamp with time zone,
+  priority integer,
+  supported_vehicle_mode text
+  )
+RENAME TO insert_scheduled_stop_point;
+
+
+DROP TRIGGER service_pattern_insert_scheduled_stop_point_trigger
+  ON service_pattern.scheduled_stop_point;
+
+DROP FUNCTION service_pattern.insert_scheduled_stop_point ();
+
+ALTER FUNCTION deleted.insert_scheduled_stop_point_1654783185212 ()
+  SET SCHEMA service_pattern;
+
+ALTER FUNCTION service_pattern.insert_scheduled_stop_point_1654783185212 ()
+  RENAME TO insert_scheduled_stop_point;
+
+CREATE TRIGGER service_pattern_insert_scheduled_stop_point_trigger
+  INSTEAD OF INSERT ON service_pattern.scheduled_stop_point
+  FOR EACH ROW EXECUTE PROCEDURE service_pattern.insert_scheduled_stop_point();
