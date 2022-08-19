28aa5e1f24ba21bbb2c61643eb8a62f3db680603
Henrik Hartiala <henrik.hartiala@vincit.fi>
2022-08-19 13:38:22 +0300

    Change route_shape calculation to use ST_MakeLine

---
+++ migrations/generic/default/1660903023968_change_route_shape_calculation_to_use_make_line/down.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION route.route_shape(route_row route.route)
+RETURNS geography AS $route_shape$
+  SELECT
+    ST_LineMerge(
+    ST_Collect(
+      CASE
+        WHEN ilar.is_traversal_forwards THEN il.shape::geometry
+        ELSE ST_Reverse(il.shape::geometry)
+        END
+      )
+    )::geography AS route_shape
+  FROM
+    route.route r
+      LEFT JOIN (
+      route.infrastructure_link_along_route AS ilar
+        INNER JOIN infrastructure_network.infrastructure_link AS il
+        ON (ilar.infrastructure_link_id = il.infrastructure_link_id)
+      ) ON (route_row.route_id = ilar.route_id)
+    WHERE r.route_id = route_row.route_id;
+$route_shape$ LANGUAGE sql STABLE;
