785cd885fdba3307c3627c7e1702e709a4110262
Tamas Pallos <tamas.pallos@vincit.fi>
2022-09-23 15:05:26 +0300

    Initial version of the timetables data model
    - Updated Hasura version to 2.13.0 to support namespacing.
    - Docker entrypoint has changed so that it could handle multiple database urls
    - New data source added to Hasura: timetables
    - The new Hasura version changed how invalid attributes are printed, so integration tests had to be fixed
    - Added initial timetables data model migration to timetables database
    - Added up-down migration tests for timetables database as well

---
+++ migrations/generic/timetables/1663231564515_timetables_initial/down.sql
@@ -0,0 +1,35 @@
+-------------------- Passing Times --------------------
+
+DROP TABLE passing_times.timetabled_passing_time;
+DROP SCHEMA passing_times;
+
+-------------------- Vehicle Journey --------------------
+
+DROP TABLE vehicle_journey.vehicle_journey;
+DROP SCHEMA vehicle_journey;
+
+-------------------- Vehicle Service --------------------
+
+DROP TABLE vehicle_service.block;
+DROP TABLE vehicle_service.vehicle_service;
+DROP SCHEMA vehicle_service;
+
+-------------------- Vehicle Schedule --------------------
+
+DROP TABLE vehicle_schedule.vehicle_schedule_frame;
+DROP SCHEMA vehicle_schedule;
+
+-------------------- Service Calendar --------------------
+
+DROP TABLE service_calendar.day_type;
+DROP SCHEMA service_calendar;
+
+-------------------- Service Pattern --------------------
+
+DROP TABLE service_pattern.scheduled_stop_point_in_journey_pattern_ref;
+DROP SCHEMA service_pattern;
+
+-------------------- Journey Pattern --------------------
+
+DROP TABLE journey_pattern.journey_pattern_ref;
+DROP SCHEMA journey_pattern;
