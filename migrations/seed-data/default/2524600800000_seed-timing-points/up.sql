3a48f538a40b0784dc0903eb486bace8366be9d9
Tamas Pallos <tamas.pallos@vincit.fi>
2022-10-14 10:43:16 +0300

    Add timing points

---
+++ migrations/seed-data/default/2524600800000_seed-timing-points/up.sql
@@ -0,0 +1,33 @@
+INSERT INTO timing_pattern.timing_point (timing_point_id,label,description) VALUES
+	 ('d094b604-3860-4e2f-a601-9dad6f9827b9','1AACKT','{"fi_FI":"Aino Ackten tie"}'),
+	 ('fece6456-e6fe-4a44-8c61-ee0a6031d799','1ALAKT','{"fi_FI":"Alakiventie"}'),
+	 ('194e347c-670e-4f03-b645-71bfb7d05031','1ALAM','{"fi_FI":"Ala-Malmin tori"}'),
+	 ('1bd78f7f-3b13-41f9-8967-4611c8ab0026','1ALAML','{"fi_FI":"Ala-Malmi"}'),
+	 ('15ae9e77-5727-4033-a2bf-925142dd158d','1ALATI','{"fi_FI":"Ala-Tikkurila"}'),
+	 ('413e6251-fe32-4c4d-b258-f85a02793655','1ALKU','{"fi_FI":"Alkutie"}'),
+	 ('87e05267-5ba0-4bc6-b126-0ece9c47af9f','1ALPKY','{"fi_FI":"Alppikylä"}'),
+	 ('3ab0d252-48f2-435e-b07a-dff2d9ef7e11','1ALT','{"fi_FI":"Ala-Tikkurila"}'),
+	 ('cc282ec9-3894-439f-bad4-2f29c491ed99','1ARAB','{"fi_FI":"Arabia"}'),
+	 ('d1a1b9cf-c91d-407c-93bb-7fb0ec6f3e85','1ARB','{"fi_FI":"Arabianranta"}');
+INSERT INTO timing_pattern.timing_point (timing_point_id,label,description) VALUES
+	 ('061c5eb9-5641-4a45-930c-950885aadc04','1ARBKE','{"fi_FI":"Kumpulan kampus"}'),
+	 ('7d2a1082-fc86-421f-9990-47484f812a5d','1AREE2','{"fi_FI":"Areena,poistumispysäkki"}'),
+	 ('5fec7533-c281-42b2-bc75-e91616afa23b','1AREE','{"fi_FI":"Areena"}'),
+	 ('886f3fa2-3ea0-48e4-9bc4-4183f0a6f3ac','1ARKAD','{"fi_FI":"Luonnont. Mus"}'),
+	 ('77dcdda2-bfc4-4aec-b24d-0dcff317857d','1AURLA','{"fi_FI":"Aurinkolahti"}'),
+	 ('73ebe404-3f2c-450f-a678-a6d048221695','1CRU','{"fi_FI":"Crusellinsilta"}'),
+	 ('ee55cddc-0b0c-476f-91ac-eadd4ee9651b','1DEG','{"fi_FI":"Degermossa"}'),
+	 ('06e716cc-4879-4098-a24b-700ed6141072','1DIA','{"fi_FI":"Linjat"}'),
+	 ('e4820f87-a9ba-47fb-872c-5cec5270ff0e','1DIAP','{"fi_FI":"Diakoniapuisto"}'),
+	 ('f92a78a0-2f7f-4511-920d-34d2b2f01ddf','1EDITA','{"fi_FI":"Editan painotalo"}');
+INSERT INTO timing_pattern.timing_point (timing_point_id,label,description) VALUES
+	 ('8b4ef6ca-a666-48c5-926b-9412361b3a78','1EIR','{"fi_FI":"Eiran sairaala"}'),
+	 ('34a902d3-a452-4e60-a204-599e25907651','1EIRA','{"fi_FI":"Eiran sairaala"}'),
+	 ('994239bc-fb54-4825-b416-6f20e33533d7','1EL','{"fi_FI":"Eläintarha"}'),
+	 ('f72aeb0b-5ce3-47c1-bc55-0d013ac69c70','1ELIEL','{"fi_FI":"Elielinaukio"}'),
+	 ('651a693b-b18b-4daa-a6da-c3677bfd2113','1ELIMK','{"fi_FI":"Elimäenkatu"}'),
+	 ('31ace2e2-cedc-442d-aa46-e2e35a968ed1','1ELÄI','{"fi_FI":"Pohjoinen Rautatiekatu"}'),
+	 ('bceb3555-6aae-4000-8917-47d3d1432895','1ERA','{"fi_FI":"Eiranranta"}'),
+	 ('99f9abf6-8f36-4079-b0b4-e0d8f73e7c2d','1EROTT','{"fi_FI":"Erottaja"}'),
+	 ('2843d31b-b8e0-4e39-81b7-042612d5722f','1ET','{"fi_FI":"Eteläranta"}'),
+	 ('68026217-3e2a-45cc-b9b9-38e11e375a89','1ETOR','{"fi_FI":"Eränkävijäntori"}');
