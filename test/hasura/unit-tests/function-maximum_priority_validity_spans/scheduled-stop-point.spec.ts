4c99e3d92211e060b9274cc2b1db2e42ffcea77f
Tamas Pallos <tamas.pallos@vincit.fi>
2022-10-17 13:19:09 +0300

    Remove scheduled stop point view
    It's hard to maintain both the service_pattern.scheduled_stop_point view and the internal_service_pattern.scheduled_stop_point table.
    - drop the service_pattern.scheduled_stop_point view and replace it with internal table
    - move all functions and tables from internal_service_pattern schema to service_pattern schema
    - extend the service_pattern.get_scheduled_stop_points_with_new function to also calculate the relative_distance_from_infrastructure_link_start and closest_point_on_infrastructure_link values (as it cannot reference these fields from the view anymore)
    - switch the instead-of triggers from the view to before/after triggers to maintain the scheduled_stop_point_invariant table’s labels

--- test/hasura/unit-tests/function-maximum_priority_validity_spans/scheduled-stop-point.spec.ts
+++ test/hasura/unit-tests/function-maximum_priority_validity_spans/scheduled-stop-point.spec.ts
@@ -1,6 +1,6 @@
-import * as pg from 'pg';
 import * as config from '@config';
-import '@util/matchers';
+import { buildLocalizedString } from '@datasets/factories';
+import { setupDb } from '@datasets/setup';
 import {
   InfrastructureLink,
   InfrastructureLinkProps,
@@ -14,12 +14,12 @@ import {
   ScheduledStopPointInJourneyPatternProps,
   ScheduledStopPointProps,
 } from '@datasets/types';
-import { setupDb } from '@datasets/setup';
 import * as db from '@util/db';
-import { randomUUID } from 'crypto';
-import { buildLocalizedString } from '@datasets/factories';
-import { LocalDate } from 'local-date';
 import { nextDay, prevDay } from '@util/helpers';
+import '@util/matchers';
+import { randomUUID } from 'crypto';
+import { LocalDate } from 'local-date';
+import * as pg from 'pg';
 
 const defaultRouteLabel = 'route 2';
 const stopLabel = 'stop A';
@@ -102,7 +102,7 @@ describe('Function maximum_priority_validity_spans should return correct schedul
           data: [infraLink],
         },
         {
-          name: 'internal_service_pattern.scheduled_stop_point',
+          name: 'service_pattern.scheduled_stop_point',
           props: ScheduledStopPointProps,
           data: stopData,
         },
