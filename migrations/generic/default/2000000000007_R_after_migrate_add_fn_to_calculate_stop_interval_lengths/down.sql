85c9de43a78ab41c39fbfc72f520771891f1f4b5
Jarkko Kaura <jarkko.kaura@vincit.fi>
2022-11-11 16:56:46 +0200

    Add SQL function to the `default` database to calculate distances between scheduled stop points.

---
+++ migrations/generic/default/2000000000007_R_after_migrate_add_fn_to_calculate_stop_interval_lengths/down.sql
@@ -0,0 +1,4 @@
+DROP FUNCTION service_pattern.get_distances_between_stop_points_for_journey_patterns(uuid, integer, date);
+DROP FUNCTION service_pattern.get_distances_between_stop_points(uuid[], date);
+DROP FUNCTION service_pattern.find_scheduled_stop_point_locations_in_journey_pattern(uuid, date);
+DROP FUNCTION route.find_route_ids(uuid, int, date);
