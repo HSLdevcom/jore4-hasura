11da6ad81ee45990ced0885b1d1d43833711be35
Tamas Pallos <tamas.pallos@vincit.fi>
2022-11-24 13:01:41 +0200

    Add functions to find active timetable on given observation date
    - also updated hasura metadata to make the sql functions callable from graphql api
    - also updated how yq is installed in the Dockerfile as the previous installation was broken
    - also made a more generic approach for merging metadata that also works for timetables

---
+++ migrations/hsl/timetables/2000000000101_R_after_migrate_create_vehicle_service/up.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION vehicle_service.get_vehicle_services_for_date(observation_date date)
+  RETURNS SETOF vehicle_service.vehicle_service
+  LANGUAGE sql STABLE
+AS $$
+  SELECT vs.*
+    FROM vehicle_service.vehicle_service vs
+    JOIN service_calendar.get_active_day_types_for_date(observation_date) dt on vs.day_type_id = dt.day_type_id
+    JOIN vehicle_schedule.vehicle_schedule_frame vsf on vs.vehicle_schedule_frame_id = vsf.vehicle_schedule_frame_id
+    -- match only effective vehicle schedule frames on the given operating day
+    WHERE observation_date BETWEEN vsf.validity_start AND vsf.validity_end
+$$;
+COMMENT ON FUNCTION vehicle_service.get_vehicle_services_for_date IS 'Find all vehicle services that are active on the given observation date.
+The results are not filtered by highest priority, that can be done on the UI on demand';
