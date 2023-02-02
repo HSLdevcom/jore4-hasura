fa8ce52dc7e89dd8354bf4c581e94bd4dd55b5ef
Tamas Pallos <tamas.pallos@vincit.fi>
2023-02-02 15:25:04 +0200

    Create example timetablesdb integration test
    Closes https://github.com/HSLdevcom/jore4/issues/1135

---
+++ test/hasura/generic/timetablesdb/datasets/factories.ts
@@ -0,0 +1,33 @@
+import { buildLocalizedString } from '@util/dataset';
+import { LocalDate } from 'local-date';
+import { TimetablePriority, VehicleScheduleFrame } from './types';
+
+// can set either name_i18n (-> leave as is) or name (-> make i18n version)
+type EntityName =
+  | { name?: never; name_i18n: LocalizedString }
+  | { name: string; name_i18n?: never };
+
+/**
+ * Builds the name for the entity if the i18n attribute is not set
+ */
+const buildName = <TEntity extends EntityName>(entityWithName: TEntity) =>
+  entityWithName.name_i18n || buildLocalizedString(entityWithName.name);
+
+export const buildVehicleScheduleFrame = (
+  frame: RequiredKeys<
+    Partial<VehicleScheduleFrame>,
+    'vehicle_schedule_frame_id'
+  > &
+    EntityName,
+) => {
+  const vehicleFrame = {
+    validity_start: new LocalDate('2020-01-01'),
+    validity_end: new LocalDate('2050-12-31'),
+    priority: TimetablePriority.Standard,
+    ...frame,
+    name_i18n: buildName(frame),
+  };
+  // remove the dangling 'name' attribute as it causes issues when inserting it to database
+  delete vehicleFrame.name;
+  return vehicleFrame;
+};
