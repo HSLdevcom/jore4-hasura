5034a4e4803883b0c7987c343cf5f2a4775c7e1d
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-02-09 13:29:48 +0200

    Add a base dataset for timetables tests

---
+++ test/hasura/generic/timetablesdb/datasets/defaultSetup/day-types.ts
@@ -0,0 +1,12 @@
+// These are populated in migrations.
+export const defaultDayTypeIds = {
+  MONDAY_THURSDAY: '6781bd06-08cf-489e-a2bb-be9a07b15752',
+  MONDAY_FRIDAY: '38853b0d-ec36-4110-b4bc-f53218c6cdcd',
+  MONDAY: 'd3dfb71f-8ee1-41fd-ad49-c4968c043290',
+  TUESDAY: 'c1d27421-dd3b-43b6-a0b9-7387aae488c9',
+  WEDNESDAY: '5ec086a3-343c-42f0-a050-3464fc3d63de',
+  THURSDAY: '9c708e58-fb49-440e-b4bd-736b9275f53f',
+  FRIDAY: '7176e238-d46e-4583-a567-b836b1ae2589',
+  SATURDAY: '61374d2b-5cce-4a7d-b63a-d487f3a05e0d',
+  SUNDAY: '0e1855f1-dfca-4900-a118-f608aa07e939',
+};
--- test/hasura/generic/timetablesdb/datasets/types.ts
+++ test/hasura/generic/timetablesdb/datasets/types.ts
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 
 export type UUID = string;
 
@@ -26,4 +26,82 @@ export const vehicleScheduleFrameProps: Property[] = [
   'validity_start',
   'validity_end',
   'priority',
+];
+
+export type VehicleService = {
+  vehicle_service_id: UUID;
+  day_type_id: UUID;
+  vehicle_schedule_frame_id: UUID;
+};
+
+export const vehicleServiceProps: Property[] = [
+  'vehicle_service_id',
+  'day_type_id',
+  'vehicle_schedule_frame_id',
+];
+
+export type VehicleServiceBlock = {
+  block_id: UUID;
+  vehicle_service_id: UUID;
+};
+
+export const vehicleServiceBlockProps: Property[] = [
+  'block_id',
+  'vehicle_service_id',
+];
+
+export type VehicleJourney = {
+  vehicle_journey_id: UUID;
+  block_id: UUID;
+  journey_pattern_ref_id: UUID;
+};
+
+export const vehicleJourneyProps: Property[] = [
+  'vehicle_journey_id',
+  'block_id',
+  'journey_pattern_ref_id',
+];
+
+export type TimetabledPassingTime = {
+  timetabled_passing_time_id: UUID;
+  arrival_time: Duration | null;
+  departure_time: Duration | null;
+  vehicle_journey_id: UUID;
+  scheduled_stop_point_in_journey_pattern_ref_id: UUID;
+};
+
+export const timetabledPassingTimeProps: Property[] = [
+  'timetabled_passing_time_id',
+  'arrival_time',
+  'departure_time',
+  'vehicle_journey_id',
+  'scheduled_stop_point_in_journey_pattern_ref_id',
+];
+
+export type ScheduledStopInJourneyPatternRef = {
+  scheduled_stop_point_in_journey_pattern_ref_id: UUID;
+  scheduled_stop_point_label: string;
+  scheduled_stop_point_sequence: number;
+  journey_pattern_ref_id: UUID;
+};
+
+export const scheduledStopInJourneyPatternRefProps: Property[] = [
+  'scheduled_stop_point_in_journey_pattern_ref_id',
+  'scheduled_stop_point_label',
+  'scheduled_stop_point_sequence',
+  'journey_pattern_ref_id',
+];
+
+export type JourneyPatternRef = {
+  journey_pattern_ref_id: UUID;
+  observation_timestamp: DateTime;
+  snapshot_timestamp: DateTime;
+  journey_pattern_id: UUID;
+};
+
+export const journeyPatternRefProps: Property[] = [
+  'journey_pattern_ref_id',
+  'observation_timestamp',
+  'snapshot_timestamp',
+  'journey_pattern_id',
 ];
