50c8e844ef725ebd9b746efaef04a183ec64e707
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-02-27 14:45:00 +0200

    Return date times from knex as luxon DateTimes and add default timezone for luxon
    Merely adding the luxon default timezone was not quite enough:
    broke on CI, which uses utc -> pg returned JS dates in UTC
    (DB itself returns them as YYYY-MM-DD strings)
    which did not match luxon dates they were compared with.
    
    Now all luxon objects are in Finland timezone,
    and tests still work no matter what timezone the runner is.
    
    Some related links:
    https://github.com/knex/knex/issues/3071
    https://node-postgres.com/features/types#date--timestamp--timestamptz
    https://github.com/brianc/node-pg-types/issues/138

--- test/hasura/config.ts
+++ test/hasura/config.ts
@@ -1,12 +1,19 @@
 import * as dotenv from 'dotenv';
 import { Knex } from 'knex';
-import { Settings } from 'luxon';
-import { ConnectionConfig } from 'pg';
+import { DateTime, Settings } from 'luxon';
+import { ConnectionConfig, types } from 'pg';
 
 dotenv.config({ path: process.env.DOTENV_PATH || '../.env' });
 
 // Configure Luxon.
 Settings.throwOnInvalid = true; // Invalid dates are too easy to miss otherwise.
+Settings.defaultZone = 'Europe/Helsinki';
+
+// Set global pg config: return date(time)s as luxon DateTime objects instead of JS Date.
+const parseToLuxonDateTime = (value: string) => DateTime.fromISO(value);
+types.setTypeParser(types.builtins.DATE, parseToLuxonDateTime);
+types.setTypeParser(types.builtins.TIMESTAMP, parseToLuxonDateTime);
+types.setTypeParser(types.builtins.TIMESTAMPTZ, parseToLuxonDateTime);
 
 export const networkDbConfig: ConnectionConfig = {
   host: '127.0.0.1',
