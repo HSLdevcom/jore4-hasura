715c1a1d486e044af32247dec0fb110367b2ae8b
Tamas Pallos <tamas.pallos@vincit.fi>
2023-03-02 10:32:30 +0200

    Refactor scripts to use jore4-tools instead
    The github actions and download scripts have been moved from jore4-flux and jore4-ui to jore4-tools repo

--- scripts/start_dependencies.sh
+++ scripts/start_dependencies.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-# based on https://github.com/HSLdevcom/jore4-flux#docker-compose
+# based on https://github.com/HSLdevcom/jore4-tools#download-docker-bundlesh
 
 set -euo pipefail
 
-# allow running from any working directory
-WD=$(dirname "$0")
-cd "${WD}"
-
-if ! command -v gh; then
-  echo "Please install the github gh tool on your machine."
-  exit 1
-fi
-
-# initialize package folder
-mkdir -p ../docker/update
-
 echo "Downloading latest version of E2E docker-compose package..."
-gh auth status || gh auth login
-
-# gh cannot overwrite existing files, therefore first download into separate dir. This way we still have the old copy
-# in case the download fails
-rm -rf ../docker/update/*
-gh release download e2e-docker-compose --repo HSLdevcom/jore4-flux --dir ../docker/update
-cp -R ../docker/update/* ../docker
+curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
 
 # start up test database and hasura for migrations
-docker-compose -f ../docker/docker-compose.yml -f ../docker/docker-compose.custom.yml up --build "$@" jore4-testdb jore4-hasura jore4-auth
+docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml up --build "$@" jore4-testdb jore4-hasura jore4-auth
