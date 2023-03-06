f0cf6e886d4acc5d7fe8af821739a04ad3ece345
Jarkko Kaura <jarkko.kaura@vincit.fi>
2023-03-06 19:41:47 +0200

    Move HSL-specific entities out of generic folder.

--- migrations/generic/timetables/1677064467443_add_type_of_line_to_journey_pattern_ref/down.sql
+++ migrations/generic/timetables/1677064467443_add_type_of_line_to_journey_pattern_ref/down.sql
@@ -1,7 +1,3 @@
--------------------- Service Calendar --------------------
-
-DROP TABLE service_calendar.substitute_operating_day_by_line_type;
-
 -------------------- Journey Pattern --------------------
 
 ALTER TABLE ONLY journey_pattern.journey_pattern_ref DROP COLUMN type_of_line;
