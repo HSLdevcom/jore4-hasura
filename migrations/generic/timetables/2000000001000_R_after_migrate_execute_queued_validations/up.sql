cebfb59c715224a7e85c8b69f59ee9076319a34c
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-03-17 12:37:31 +0200

    Refactor validation triggers and related queue tables
    Use same queue tables everywhere, create them and related triggers
    in one place, and run all required validations etc from one trigger.
    
    One problem that this solves is ensuring that the denormalized
    journey_patterns_in_vehicle_service table is up to date before
    any validations that depend on it are ran.
    Also focuses all verbose trigger code in one place instead of scattered
    in multiple migration files.

---
+++ migrations/generic/timetables/2000000001000_R_after_migrate_execute_queued_validations/up.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION internal_utils.execute_queued_validations() RETURNS trigger
+  LANGUAGE plpgsql
+AS $$
+BEGIN
+  -- RAISE LOG 'internal_utils.execute_queued_validations() started';
+
+  -- In case this is called without creating he tables.
+  -- At least for refresh_journey_patterns_in_vehicle_service there is such case (vehicle_journey delete)
+  PERFORM internal_utils.create_validation_queue_temp_tables();
+
+  -- RAISE LOG 'before vehicle_service.refresh_journey_patterns_in_vehicle_service()';
+  PERFORM vehicle_service.refresh_journey_patterns_in_vehicle_service();
+
+  -- RAISE LOG 'before vehicle_schedule.validate_queued_schedules_uniqueness()';
+  PERFORM vehicle_schedule.validate_queued_schedules_uniqueness();
+
+  -- RAISE LOG 'internal_utils.execute_queued_validations() finished';
+
+  RETURN NULL;
+END;
+$$;
+COMMENT ON FUNCTION internal_utils.execute_queued_validations()
+IS 'Runs all queued validations. This is intended to be ran only once per transaction (see queued_validations_already_processed).';
