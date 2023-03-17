5019dccbe9e71236f5fc73d315b802c3c485e769
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-03-17 13:38:42 +0200

    move journey_patterns_in_vehicle_service

--- migrations/generic/timetables/2000000000000_R_after_migrate_create_vehicle_journey/up.sql
+++ migrations/generic/timetables/2000000000000_R_after_migrate_create_vehicle_journey/up.sql
@@ -61,6 +61,23 @@ $$;
 COMMENT ON FUNCTION vehicle_schedule.queue_validation_by_vsf_id()
 IS 'Queue modified vehicle schedule frames for validation which is performed at the end of transaction.';
 
+CREATE OR REPLACE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh() RETURNS TRIGGER
+  LANGUAGE plpgsql
+  AS $$
+BEGIN
+  PERFORM internal_utils.create_validation_queue_temp_tables();
+
+  -- Note: just setting a global flag is enough because we refresh the whole table every time.
+  -- If this were ever to change so that only required parts are refreshed,
+  -- the queue tables should be utilized instead.
+  SET LOCAL vehicle_service.journey_patterns_in_vehicle_service_refresh_queued = TRUE;
+  RETURN NEW;
+END
+$$;
+COMMENT ON FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh()
+IS 'Sets a flag that journey_patterns_in_vehicle_service should be refreshed.
+ The actual refresh can then be triggered by execute_queued_validations()';
+
 CREATE OR REPLACE FUNCTION internal_utils.queued_validations_already_processed() RETURNS boolean
     LANGUAGE plpgsql
     AS $$
@@ -109,6 +126,33 @@ CREATE TRIGGER queue_vsf_validation_on_update_trigger
   FOR EACH STATEMENT
   EXECUTE FUNCTION vehicle_schedule.queue_validation_by_vsf_id();
 
+-- Following queue triggers are currently only for journey_patterns_in_vehicle_service refresh.
+-- Thus they don't use the queue tables but a separate global flag.
+--
+-- For INSERT/DELETE we only need to trigger refresh when vehicle_journey is touched.
+-- If any rows from tables higher in the hierarchy are INSERTed/DELETEd,
+-- then their children (vehicle_journey) will be touched as well.
+--
+-- For UPDATE operations, only foreign key column updates can invalidate
+-- the journey_patterns_in_vehicle_service table, so it is enough to check those.
+DROP TRIGGER IF EXISTS queue_refresh_jps_in_vs_on_block_modified_trigger ON vehicle_service.block;
+CREATE TRIGGER queue_refresh_jps_in_vs_on_block_modified_trigger
+  AFTER UPDATE ON vehicle_service.block
+  FOR EACH STATEMENT
+  EXECUTE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh();
+
+DROP TRIGGER IF EXISTS queue_refresh_jps_in_vs_on_vj_modified_trigger ON vehicle_journey.vehicle_journey;
+CREATE TRIGGER queue_refresh_jps_in_vs_on_vj_modified_trigger
+  AFTER UPDATE OR INSERT OR DELETE ON vehicle_journey.vehicle_journey
+  FOR EACH STATEMENT
+  EXECUTE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh();
+
+DROP TRIGGER IF EXISTS queue_refresh_jps_in_vs_on_jpr_modified_trigger ON journey_pattern.journey_pattern_ref;
+CREATE TRIGGER queue_refresh_jps_in_vs_on_jpr_modified_trigger
+  AFTER UPDATE ON journey_pattern.journey_pattern_ref
+  FOR EACH STATEMENT
+  EXECUTE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh();
+
 -- End of statement validation triggers:
 
 DROP TRIGGER IF EXISTS process_queued_validation_on_vsf_trigger ON vehicle_schedule.vehicle_schedule_frame;
@@ -119,21 +163,51 @@ CREATE CONSTRAINT TRIGGER process_queued_validation_on_vsf_trigger
   WHEN (NOT internal_utils.queued_validations_already_processed())
   EXECUTE FUNCTION internal_utils.execute_queued_validations();
 COMMENT ON TRIGGER process_queued_validation_on_vsf_trigger ON vehicle_schedule.vehicle_schedule_frame
-IS 'Trigger to validate the schedule uniqueness after modifications on vehicle schedule frames.
-    This trigger will cause those VSFs to be checked, whose ID was queued to be checked by a statement level trigger.';
+IS 'Trigger to execute queued validations that were registered earlier by statement level triggers';
+
+DROP TRIGGER IF EXISTS process_queued_validation_on_block_trigger ON vehicle_service.block;
+CREATE CONSTRAINT TRIGGER process_queued_validation_on_block_trigger
+  AFTER UPDATE ON vehicle_service.block
+  DEFERRABLE INITIALLY DEFERRED
+  FOR EACH ROW
+  WHEN (NOT internal_utils.queued_validations_already_processed())
+  EXECUTE FUNCTION internal_utils.execute_queued_validations();
+COMMENT ON TRIGGER process_queued_validation_on_block_trigger ON vehicle_service.block
+IS 'Trigger to execute queued validations that were registered earlier by statement level triggers';
+
+DROP TRIGGER IF EXISTS process_queued_validation_on_vj_trigger ON vehicle_journey.vehicle_journey;
+CREATE CONSTRAINT TRIGGER process_queued_validation_on_vj_trigger
+  AFTER UPDATE OR INSERT OR DELETE ON vehicle_journey.vehicle_journey
+  DEFERRABLE INITIALLY DEFERRED
+  FOR EACH ROW
+  WHEN (NOT internal_utils.queued_validations_already_processed())
+  EXECUTE FUNCTION internal_utils.execute_queued_validations();
+COMMENT ON TRIGGER process_queued_validation_on_vj_trigger ON vehicle_journey.vehicle_journey
+IS 'Trigger to execute queued validations that were registered earlier by statement level triggers';
+
+DROP TRIGGER IF EXISTS process_queued_validation_on_jpr_trigger ON journey_pattern.journey_pattern_ref;
+CREATE CONSTRAINT TRIGGER process_queued_validation_on_jpr_trigger
+  AFTER UPDATE ON journey_pattern.journey_pattern_ref
+  DEFERRABLE INITIALLY DEFERRED
+  FOR EACH ROW
+  WHEN (NOT internal_utils.queued_validations_already_processed())
+  EXECUTE FUNCTION internal_utils.execute_queued_validations();
+COMMENT ON TRIGGER process_queued_validation_on_jpr_trigger ON journey_pattern.journey_pattern_ref
+IS 'Trigger to execute queued validations that were registered earlier by statement level triggers';
+
 
 
 -- Triggers TODO here:
 
 -- done AFTER INSERT ON vehicle_schedule.vehicle_schedule_frame -- schedules
 -- done AFTER UPDATE ON vehicle_schedule.vehicle_schedule_frame -- schedules
--- AFTER UPDATE ON journey_pattern.journey_pattern_ref -- journey_patterns_in_vehicle_service
+-- done AFTER UPDATE ON journey_pattern.journey_pattern_ref -- journey_patterns_in_vehicle_service
 -- AFTER UPDATE ON journey_pattern.journey_pattern_ref -- schedules
--- AFTER UPDATE ON vehicle_service.block -- journey_patterns_in_vehicle_service
+-- done AFTER UPDATE ON vehicle_service.block -- journey_patterns_in_vehicle_service
 -- AFTER UPDATE ON vehicle_service.block -- foreign key -- schedules
 -- AFTER UPDATE ON vehicle_service.vehicle_service -- foreign key, days? -- schedules
 -- AFTER UPDATE OR INSERT ON vehicle_journey -- schedules
--- AFTER UPDATE OR INSERT OR DELETE ON vehicle_journey.vehicle_journey -- journey_patterns_in_vehicle_service
+-- done AFTER UPDATE OR INSERT OR DELETE ON vehicle_journey.vehicle_journey -- journey_patterns_in_vehicle_service
 
 -- -- passing times
 -- AFTER UPDATE ON passing_times.timetabled_passing_time
--- migrations/generic/timetables/2000000000003_R_add_journey_patterns_in_vehicle_service_sync_objects/up.sql
+++ migrations/generic/timetables/2000000000003_R_add_journey_patterns_in_vehicle_service_sync_objects/up.sql
@@ -2,6 +2,14 @@ ALTER TABLE vehicle_service.journey_patterns_in_vehicle_service
   ADD CONSTRAINT journey_patterns_in_vehicle_service_reference_count_check
   CHECK (reference_count >= 0);
 
+-- We expect UPDATE/INSERT/DELETE operations to be rare and mostly be done
+-- in one transaction when data is imported, so it does not matter much
+-- that we refresh the whole table on each write operation.
+--
+-- If it looks like write operations will be used more regularly,
+-- it might make sense to rethink these triggers, possibly optimizing them
+-- to only refresh necessary parts of the journey_patterns_in_vehicle_service table.
+-- The reference_count can probably be utilized for such purpose.
 CREATE OR REPLACE FUNCTION vehicle_service.refresh_journey_patterns_in_vehicle_service() RETURNS VOID
   LANGUAGE plpgsql
   VOLATILE PARALLEL UNSAFE
@@ -32,117 +40,3 @@ END;
 $$;
 COMMENT ON FUNCTION vehicle_service.refresh_journey_patterns_in_vehicle_service()
 IS 'Rebuilds the whole journey_patterns_in_vehicle_service table.';
-
-CREATE OR REPLACE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh() RETURNS TRIGGER
-  LANGUAGE plpgsql
-  AS $$
-BEGIN
-  SET LOCAL vehicle_service.journey_patterns_in_vehicle_service_refresh_queued = TRUE;
-  RETURN NEW;
-END
-$$;
-COMMENT ON FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh()
-IS 'Sets a flag that journey_patterns_in_vehicle_service should be refreshed.
- The actual refresh can then be triggered by execute_queued_journey_patterns_in_vehicle_service_refresh_once()';
-
-CREATE OR REPLACe FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_once() RETURNS VOID
-  LANGUAGE plpgsql
-  AS $$
-DECLARE
-  journey_patterns_in_vehicle_service_refresh_queued BOOLEAN;
-  already_refreshed_journey_patterns_in_vehicle_service BOOLEAN;
-BEGIN
-  -- RAISE LOG 'execute_queued_journey_patterns_in_vehicle_service_refresh_once()';
-
-  journey_patterns_in_vehicle_service_refresh_queued := NULLIF(current_setting('vehicle_service.journey_patterns_in_vehicle_service_refresh_queued', TRUE), '');
-  -- RAISE LOG 'execute_queued_journey_patterns_in_vehicle_service_refresh_once, refresh queued refreshed: %', journey_patterns_in_vehicle_service_refresh_queued;
-  IF journey_patterns_in_vehicle_service_refresh_queued IS TRUE THEN
-    already_refreshed_journey_patterns_in_vehicle_service := NULLIF(current_setting('vehicle_service.already_refreshed_journey_patterns_in_vehicle_service', TRUE), '');
-    -- RAISE LOG 'execute_queued_journey_patterns_in_vehicle_service_refresh_once, already refreshed: %', already_refreshed_journey_patterns_in_vehicle_service;
-
-    IF already_refreshed_journey_patterns_in_vehicle_service IS NOT TRUE THEN
-      -- RAISE LOG 'execute_queued_journey_patterns_in_vehicle_service_refresh_once(): execute and reset flag.';
-
-      PERFORM vehicle_service.refresh_journey_patterns_in_vehicle_service();
-
-      SET LOCAL vehicle_service.already_refreshed_journey_patterns_in_vehicle_service = TRUE;
-    END IF;
-  END IF;
-END
-$$;
-COMMENT ON FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_once()
-IS 'Executes the vehicle_service.refresh_journey_patterns_in_vehicle_service function
- if it has been queued (by queue_journey_patterns_in_vehicle_service_refresh)
- and not yet executed in this transaction via this function.
- Otherwise does nothing.';
-
-CREATE OR REPLACE FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_trg() RETURNS TRIGGER
-  LANGUAGE plpgsql
-  AS $$
-BEGIN
-  PERFORM vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_once();
-
-  RETURN NEW;
-END
-$$;
-COMMENT ON FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_trg()
-IS 'Trigger for calling execute_queued_journey_patterns_in_vehicle_service_refresh_once()';
-
--- Create triggers for initiating the journey_patterns_in_vehicle_service refresh.
--- These are created as CONSTRAINT TRIGGERs so they can be DEFERRED to the end of transaction,
--- so that all write operations are considered when refreshing the table.
---
--- We expect UPDATE/INSERT/DELETE operations to be rare and mostly be done
--- in one transaction when data is imported, so it does not matter much
--- that we refresh the whole table on each write operation.
---
--- If it looks like write operations will be used more regularly,
--- it might make sense to rethink these triggers, possibly optimizing them
--- to only refresh necessary parts of the journey_patterns_in_vehicle_service table.
--- The reference_count can probably be utilized for such purpose.
---
--- For INSERT/DELETE we only need to trigger refresh when vehicle_journey is touched.
--- If any rows from tables higher in the hierarchy are INSERTed/DELETEd,
--- then their children (vehicle_journey) will be touched as well.
---
--- For UPDATE operations, only foreign key column updates can invalidate
--- the journey_patterns_in_vehicle_service table, so it is enough to check those.
-
--- block queue + execute:
-DROP TRIGGER IF EXISTS queue_refresh_jps_in_vs_on_block_modified_trigger ON vehicle_service.block;
-CREATE TRIGGER queue_refresh_jps_in_vs_on_block_modified_trigger
-  AFTER UPDATE ON vehicle_service.block
-  FOR EACH STATEMENT
-  EXECUTE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh();
-
-DROP TRIGGER IF EXISTS refresh_jps_in_vs_on_block_modified_trigger ON vehicle_service.block;
-CREATE CONSTRAINT TRIGGER refresh_jps_in_vs_on_block_modified_trigger
-  AFTER UPDATE ON vehicle_service.block
-  DEFERRABLE INITIALLY DEFERRED FOR EACH ROW
-  EXECUTE FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_trg();
-
--- journey_pattern_ref queue + execute:
-DROP TRIGGER IF EXISTS queue_refresh_jps_in_vs_on_jpr_modified_trigger ON journey_pattern.journey_pattern_ref;
-CREATE TRIGGER queue_refresh_jps_in_vs_on_jpr_modified_trigger
-  AFTER UPDATE ON journey_pattern.journey_pattern_ref
-  FOR EACH STATEMENT
-  EXECUTE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh();
-
-DROP TRIGGER IF EXISTS refresh_jps_in_vs_on_jpr_modified_trigger ON journey_pattern.journey_pattern_ref;
-CREATE CONSTRAINT TRIGGER refresh_jps_in_vs_on_jpr_modified_trigger
-  AFTER UPDATE ON journey_pattern.journey_pattern_ref
-  DEFERRABLE INITIALLY DEFERRED FOR EACH ROW
-  EXECUTE FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_trg();
-
--- vehicle_journey queue + execute:
-DROP TRIGGER IF EXISTS queue_refresh_jps_in_vs_on_vj_modified_trigger ON vehicle_journey.vehicle_journey;
-CREATE TRIGGER queue_refresh_jps_in_vs_on_vj_modified_trigger
-  AFTER UPDATE OR INSERT OR DELETE ON vehicle_journey.vehicle_journey
-  FOR EACH STATEMENT
-  EXECUTE FUNCTION vehicle_service.queue_journey_patterns_in_vehicle_service_refresh();
-
-DROP TRIGGER IF EXISTS refresh_jps_in_vs_on_vj_modified_trigger ON vehicle_journey.vehicle_journey;
-CREATE CONSTRAINT TRIGGER refresh_jps_in_vs_on_vj_modified_trigger
-  AFTER UPDATE OR INSERT OR DELETE ON vehicle_journey.vehicle_journey
-  DEFERRABLE INITIALLY DEFERRED FOR EACH ROW
-  EXECUTE FUNCTION vehicle_service.execute_queued_journey_patterns_in_vehicle_service_refresh_trg();
