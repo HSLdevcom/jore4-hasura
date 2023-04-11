a6bc06ca85139caff79c29b26174567b24603863
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-04-11 16:07:48 +0300

    Use the BETWEEN operator in municipality code check
    Shorter, and now more consistent with other similar constraints.

--- migrations/hsl/default/2000000001001_R_after_migrate_route/up.sql
+++ migrations/hsl/default/2000000001001_R_after_migrate_route/up.sql
@@ -2,4 +2,4 @@ ALTER TABLE route.route
   ADD CONSTRAINT route_variant_unsigned_check CHECK (variant >= 0);
 
 ALTER TABLE hsl_route.legacy_hsl_municipality_code
-  ADD CONSTRAINT hsl_route_legacy_hsl_muhnicipality_code_digit_check CHECK (0 <= jore3_code AND jore3_code <= 9);
+  ADD CONSTRAINT hsl_route_legacy_hsl_muhnicipality_code_digit_check CHECK (jore3_code BETWEEN 0 AND 9);
