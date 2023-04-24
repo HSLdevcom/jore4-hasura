bb0b0c40be54c0a56fda741983706f7b8d8025aa
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-04-24 16:55:32 +0300

    Add timetable version return value table
    This introduces the schema return_value which is used to have all the
    tables that will not contain any data, but are used for function return
    values as they are required to have. There are already this type of
    return value tables in the database that are not located in this scheme,
    but they need to be moved under this schema in the future. Although
    this commit will add timetable_version as the first return_value.

---
+++ migrations/hsl/timetables/1679393480845_add_timetable_version_return_value/up.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE return_value.timetable_version (
+  vehicle_schedule_frame_id uuid REFERENCES vehicle_schedule.vehicle_schedule_frame(vehicle_schedule_frame_id) NULL,
+  substitute_operating_day_by_line_type_id uuid REFERENCES service_calendar.substitute_operating_day_by_line_type(substitute_operating_day_by_line_type_id) NULL,
+  validity_start date NOT NULL,
+  validity_end date NOT NULL,
+  priority integer NOT NULL,
+  in_effect boolean NOT NULL,
+  day_type_id uuid REFERENCES service_calendar.day_type(day_type_id) NULL
+);
+
+COMMENT ON TABLE return_value.timetable_version IS 'This return value is used for functions that determine what timetable versions are in effect.';
