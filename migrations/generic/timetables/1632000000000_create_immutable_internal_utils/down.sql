a9b63a1cbfdd185a667ff745630699c939fdfdea
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-04-24 16:56:54 +0300

    Add get timetable version functions
    This function set is used to get timetable versions and their
    effectiveness during a given time range and observation date.

--- migrations/generic/timetables/1632000000000_create_immutable_internal_utils/down.sql
+++ migrations/generic/timetables/1632000000000_create_immutable_internal_utils/down.sql
@@ -1,1 +1,7 @@
+DROP FUNCTION IF EXISTS internal_utils.const_timetables_priority_special ();
+
+DROP FUNCTION IF EXISTS internal_utils.const_timetables_priority_substitute_by_line_type ();
+
+DROP FUNCTION IF EXISTS internal_utils.const_timetables_priority_staging ();
+
 DROP FUNCTION IF EXISTS internal_utils.const_timetables_priority_draft ();
