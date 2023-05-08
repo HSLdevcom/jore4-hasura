3f8f9a0c682fb3c7249d427230444b86f1dff960
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-05-08 21:19:18 +0300

    Add a script for setting docker target
    For easily switching which version of docker image is built.

---
+++ scripts/set-docker-target.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ "$PWD" != *scripts ]]; then
+  cd "$(dirname "$0")"
+fi
+
+if [[ -z ${1} ]]; then
+  echo "Usage: set-docket-target.sh environment"
+  echo "Possible environment values are currently:"
+  ls -1 -d ../migrations/*/ | cut -f3 -d'/'
+  exit 1
+fi
+
+set -euo pipefail
+
+sed -i "s/target: hasura-.*/target: hasura-${1}/i" ../docker/docker-compose.custom.yml
