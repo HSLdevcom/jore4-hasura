ded67cb83ea24fe40dc80451147705f3b6deb989
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-05-08 21:56:32 +0300

    Add script for executing all necessary parts of generating dump files
    Including committing them.
    This can then easily be used in git rebase --exec

--- scripts/stop-dependencies.sh
+++ scripts/stop-dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# allow running from any working directory
+WD=$(dirname "$0")
+cd "${WD}/.."
+
 # stop the dependencies
 docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml down
