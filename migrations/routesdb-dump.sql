38488ce0b092df2603b3699afc7196eed8dd32ef
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-05-10 12:22:37 +0300

    Simplify a WHERE clause in new_scheduled_stop_point_if_id_given

--- migrations/routesdb-dump.sql
+++ migrations/routesdb-dump.sql
@@ -3614,13 +3614,8 @@ CREATE FUNCTION service_pattern.new_scheduled_stop_point_if_id_given(new_schedul
          internal_utils.st_linelocatepoint(il.shape, new_measured_location) AS relative_distance_from_infrastructure_link_start,
          NULL::geography(PointZ, 4326)                                      AS closest_point_on_infrastructure_link
   FROM infrastructure_network.infrastructure_link il
-  WHERE
-    CASE
-      WHEN new_scheduled_stop_point_id IS NOT NULL THEN
-        il.infrastructure_link_id = new_located_on_infrastructure_link_id
-      ELSE
-        false
-    END;
+  WHERE new_scheduled_stop_point_id IS NOT NULL
+  AND new_located_on_infrastructure_link_id = il.infrastructure_link_id;
 $$;
 
 
