8f91d810ce714874a3c90fca00fbebdc379e3b72
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-05-16 13:30:08 +0300

    Add hsl timetables test infra
    We already have generic - network, generic - timetables and hsl - network
    infra created. This commit will bring the base for hsl - timetables test infra.
    
    Resolves HSLdevcom/jore4#1275

---
+++ test/hasura/hsl/timetablesdb/integration-tests/hsl-timetables-db-default-dataset-insertion.spec.ts
@@ -0,0 +1,24 @@
+import * as config from '@config';
+import * as db from '@util/db';
+import { closeDbConnection, createDbConnection, DbConnection } from '@util/db';
+import { setupDb } from '@util/setup';
+import { defaultHslTimetablesDbData } from 'hsl/timetablesdb/datasets/defaultSetup';
+
+describe('Function service_pattern.get_distances_between_stop_points_in_journey_patterns', () => {
+  let dbConnection: DbConnection;
+
+  beforeAll(() => {
+    dbConnection = createDbConnection(config.timetablesDbConfig);
+  });
+
+  afterAll(() => closeDbConnection(dbConnection));
+
+  it('should insert defaultHslTimetablesDbData succesfully and fetch data from hsl timetables specific table', async () => {
+    await setupDb(dbConnection, defaultHslTimetablesDbData);
+    const response = await db.singleQuery(
+      dbConnection,
+      `SELECT * FROM service_calendar.substitute_operating_day_by_line_type`,
+    );
+    expect(response.rows.length).toBeGreaterThan(0);
+  });
+});
