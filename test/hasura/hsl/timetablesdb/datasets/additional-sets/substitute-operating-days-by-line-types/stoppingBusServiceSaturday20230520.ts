e9d54e54730256272afb24c886562fb1e0f6526f
Jarno Perholehto <jarno.perholehto@vincit.fi>
2023-05-31 09:54:31 +0300

    Add substitute operating period
    - New table service_calendar.substitute_operating_period to group substitute_operating_days
    - New view service_calendar.substitute_operating_period_with_date_range to get periods with date range and linetypes
    - New function service_calendar.save_operating_periods handles saving/updating substitute_operating_days for substitute_operating_periods
    - Fix integration-tests for vehicle schedules

--- test/hasura/hsl/timetablesdb/datasets/additional-sets/substitute-operating-days-by-line-types/stoppingBusServiceSaturday20230520.ts
+++ test/hasura/hsl/timetablesdb/datasets/additional-sets/substitute-operating-days-by-line-types/stoppingBusServiceSaturday20230520.ts
@@ -8,10 +8,21 @@ export const stoppingBusServiceSaturday20230520SubstituteOperatingDayByLineType 
       '88fbc283-9552-45d2-9365-60272f1e44f6',
     type_of_line: TypeOfLine.StoppingBusService,
     superseded_date: DateTime.fromISO('2023-05-20'),
+    substitute_operating_period_id: 'cfa58eec-2e5a-4ab7-a8e9-a92a46316640',
   };
+
+export const stoppingBusServiceSaturday20230520SubstituteOperatingPeriod = {
+  substitute_operating_period_id: 'cfa58eec-2e5a-4ab7-a8e9-a92a46316640',
+  is_preset: false,
+  period_name: 'Unusual Saturday for Stopping Bus Service',
+};
 
 export const stoppingBusServiceSaturday20230520Dataset: TableData<HslTimetablesDbTables>[] =
   [
+    {
+      name: 'service_calendar.substitute_operating_period',
+      data: [stoppingBusServiceSaturday20230520SubstituteOperatingPeriod],
+    },
     {
       name: 'service_calendar.substitute_operating_day_by_line_type',
       data: [
