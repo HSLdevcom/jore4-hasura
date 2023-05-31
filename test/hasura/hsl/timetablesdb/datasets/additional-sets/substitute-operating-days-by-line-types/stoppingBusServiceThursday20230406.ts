3dd850581e68f3582a25d82478ff77fcdc5fe0be
Jarno Perholehto <jarno.perholehto@vincit.fi>
2023-05-31 09:54:31 +0300

    Add substitute operating period
    - New table service_calendar.substitute_operating_period to group substitute_operating_days
    - New view service_calendar.substitute_operating_period_with_date_range to get periods with date range and linetypes
    - New function service_calendar.save_operating_periods handles saving/updating substitute_operating_days for substitute_operating_periods
    - Fix integration-tests for vehicle schedules

--- test/hasura/hsl/timetablesdb/datasets/additional-sets/substitute-operating-days-by-line-types/stoppingBusServiceThursday20230406.ts
+++ test/hasura/hsl/timetablesdb/datasets/additional-sets/substitute-operating-days-by-line-types/stoppingBusServiceThursday20230406.ts
@@ -10,10 +10,21 @@ export const stoppingBusServiceThursday20230406SubstituteOperatingDayByLineType 
     type_of_line: TypeOfLine.StoppingBusService,
     superseded_date: DateTime.fromISO('2023-04-06'),
     substitute_day_of_week: DayOfWeek.Sunday,
+    substitute_operating_period_id: 'c23c96ba-2ef5-4e0c-a3fd-9e514432d638',
   };
+
+export const stoppingBusServiceThursday20230406SubstituteOperatingPeriod = {
+  substitute_operating_period_id: 'c23c96ba-2ef5-4e0c-a3fd-9e514432d638',
+  is_preset: false,
+  period_name: "Joe Biden's Visit",
+};
 
 export const stoppingBusServiceSaturday20230406Dataset: TableData<HslTimetablesDbTables>[] =
   [
+    {
+      name: 'service_calendar.substitute_operating_period',
+      data: [stoppingBusServiceThursday20230406SubstituteOperatingPeriod],
+    },
     {
       name: 'service_calendar.substitute_operating_day_by_line_type',
       data: [
