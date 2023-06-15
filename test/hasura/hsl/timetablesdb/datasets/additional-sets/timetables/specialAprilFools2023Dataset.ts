4598d6a035f65438b24f98812f75572995dca0e2
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-06-15 12:52:51 +0300

    create passing times

--- test/hasura/hsl/timetablesdb/datasets/additional-sets/timetables/specialAprilFools2023Dataset.ts
+++ test/hasura/hsl/timetablesdb/datasets/additional-sets/timetables/specialAprilFools2023Dataset.ts
@@ -1,6 +1,7 @@
 import {
   defaultDayTypeIds,
   journeyPatternRefsByName,
+  scheduledStopPointsInJourneyPatternRef,
 } from 'generic/timetablesdb/datasets/defaultSetup';
 import { TimetablePriority } from 'generic/timetablesdb/datasets/types';
 import { DateTime } from 'luxon';
@@ -70,16 +71,83 @@ export const specialAprilFools2023Timetable: TimetablesDataset = {
         _vehicle_journeys: [
           {
             vehicle_journey_id: 'cc0ca525-eb96-462e-9658-35cd91eaa17a',
-            journey_pattern_ref_id:
-              journeyPatternRefsByName.route123Inbound.journey_pattern_ref_id,
+            _journey_pattern_ref_name: 'route123Inbound',
+
+            _passing_times: [
+              {
+                arrival_time: null,
+                departure_time: 'PT7H5M',
+                _scheduled_stop_point_label: 'H2201',
+              },
+              {
+                arrival_time: 'PT7H15M',
+                departure_time: 'PT7H15M',
+                _scheduled_stop_point_label: 'H2202',
+              },
+              {
+                arrival_time: 'PT7H22M',
+                departure_time: 'PT7H22M',
+                _scheduled_stop_point_label: 'H2203',
+              },
+              {
+                arrival_time: 'PT7H27M',
+                departure_time: null,
+                _scheduled_stop_point_label: 'H2204',
+              },
+            ],
           },
           {
             vehicle_journey_id: '5fcabadd-ee62-4455-904b-c652c14b57f0',
-            journey_pattern_ref_id:
-              journeyPatternRefsByName.route123Outbound.journey_pattern_ref_id,
+            _journey_pattern_ref_name: 'route123Outbound',
+
+            _passing_times: [
+              {
+                arrival_time: null,
+                departure_time: 'PT7H5M',
+                _scheduled_stop_point_label: 'H2201',
+              },
+              {
+                arrival_time: 'PT7H15M',
+                departure_time: 'PT7H15M',
+                _scheduled_stop_point_label: 'H2202',
+              },
+              {
+                arrival_time: 'PT7H19M',
+                departure_time: 'PT7H19M',
+                _scheduled_stop_point_label: 'H2203',
+              },
+              {
+                arrival_time: 'PT7H25M',
+                departure_time: null,
+                _scheduled_stop_point_label: 'H2204',
+              },
+            ],
           },
         ],
       },
+    },
+  },
+  _journey_patterns_by_name: {
+    route123Inbound: {
+      journey_pattern_ref_id:
+        journeyPatternRefsByName.route123Inbound.journey_pattern_ref_id,
+
+      // TODO: inline these.
+      _stop_points: scheduledStopPointsInJourneyPatternRef.filter(
+        (sp) =>
+          sp.journey_pattern_ref_id ===
+          journeyPatternRefsByName.route123Inbound.journey_pattern_ref_id,
+      ),
+    },
+    route123Outbound: {
+      journey_pattern_ref_id:
+        journeyPatternRefsByName.route123Outbound.journey_pattern_ref_id,
+
+      _stop_points: scheduledStopPointsInJourneyPatternRef.filter(
+        (sp) =>
+          sp.journey_pattern_ref_id ===
+          journeyPatternRefsByName.route123Outbound.journey_pattern_ref_id,
+      ),
     },
   },
 };
--- test/hasura/timetables-data-inserter/index.ts
+++ test/hasura/timetables-data-inserter/index.ts
@@ -1,12 +1,27 @@
 /* eslint-disable no-underscore-dangle */
 import { randomUUID } from 'crypto';
-import { VehicleJourney, VehicleService, VehicleServiceBlock } from 'generic/timetablesdb/datasets/types';
+import {
+  ScheduledStopInJourneyPatternRef,
+  TimetabledPassingTime,
+  VehicleJourney,
+  VehicleService,
+  VehicleServiceBlock,
+} from 'generic/timetablesdb/datasets/types';
 import { HslTimetablesDbTables } from 'hsl/timetablesdb/datasets/schema';
 import { HslVehicleScheduleFrame } from 'hsl/timetablesdb/datasets/types';
-import { cloneDeep } from 'lodash';
+import { cloneDeep, omit } from 'lodash';
+import { Duration } from 'luxon';
+
+export type PassingTimeInput = Partial<TimetabledPassingTime> & {
+  _scheduled_stop_point_label: string;
+  arrival_time: string | null;
+  departure_time: string | null;
+};
 
 export type VehicleJourneyInput = Partial<VehicleJourney> & {
-  // TODO
+  _journey_pattern_ref_name?: string; // Required for input, missing from output. TODO: split types.
+
+  _passing_times: PassingTimeInput[];
 };
 
 export type VehicleServiceBlockInput = Partial<VehicleServiceBlock> & {
@@ -24,30 +39,46 @@ export type VehicleScheduleFrameInput = Partial<HslVehicleScheduleFrame> & {
   _vehicle_service?: VehicleServiceInput;
   _vehicle_services?: VehicleServiceInput[];
 };
+
+export type JourneyPatternsByNameInput = Record<
+  string,
+  {
+    journey_pattern_ref_id?: UUID;
+    _stop_points: ScheduledStopInJourneyPatternRef[];
+  }
+>;
+
 export type TimetablesDataset = {
   _vehicle_schedule_frame: VehicleScheduleFrameInput;
+  _journey_patterns_by_name: JourneyPatternsByNameInput;
 };
 
+// TODO: some better approach than global variable.
+let datasetInput: TimetablesDataset | null = null;
 
-const assignId = (item: Record<string, any>, idField: string) => {
+const assignId = <T, K extends keyof T>(item: T, idField: K): T => {
   if (!item[idField]) {
     return item;
   }
 
   return {
     ...item,
-    [idField]: randomUUID(),
+    [idField]: randomUUID() as T[K],
   };
 };
 
-const assignParentId = (item: Record<string, any>, parentIdField: string, parentId: UUID) => {
-  if (item[parentIdField]) {
-    throw new Error('Parent id field already assigned');
+const assignForeignKey = <T, K extends keyof T>(
+  item: T,
+  fieldName: K,
+  id: UUID,
+): T => {
+  if (item[fieldName]) {
+    throw new Error(`Foreign key ${fieldName.toString()} already assigned`);
   }
 
   return {
     ...item,
-    [parentIdField]: parentId,
+    [fieldName]: id as T[K],
   };
 };
 
@@ -76,7 +107,7 @@ const processChildren = ({
   delete result[singularField];
 
   result[pluralField] = result[pluralField].map((child: any) => {
-    return assignParentId(child, parentIdField, item[parentIdField]);
+    return assignForeignKey(child, parentIdField, item[parentIdField]);
   });
   result[pluralField] = result[pluralField].map(mapper);
 
@@ -88,8 +119,40 @@ const processVehicleJourney = (vehicleJourney: VehicleJourneyInput) => {
 
   const idField = 'vehicle_journey_id';
   result = assignId(vehicleJourney, idField);
+  const journeyPatternRefName =
+    result._journey_pattern_ref_name || 'TODO: how to handle this';
+  const journeyPatternRef =
+    datasetInput!._journey_patterns_by_name[journeyPatternRefName];
+  result = assignForeignKey(
+    vehicleJourney,
+    'journey_pattern_ref_id',
+    journeyPatternRef.journey_pattern_ref_id as UUID,
+  );
+  delete result._journey_pattern_ref_name;
 
-  return result;
+  const stopPoints = journeyPatternRef._stop_points;
+  const processedPassingTimes = result._passing_times.map((pt) => {
+    const matchingStopPoint = stopPoints.find(
+      (sp) => sp.scheduled_stop_point_label === pt._scheduled_stop_point_label,
+    );
+
+    return {
+      ...omit(pt, ['_scheduled_stop_point_label']),
+      vehicle_journey_id: result.vehicle_journey_id,
+      scheduled_stop_point_in_journey_pattern_ref_id:
+        matchingStopPoint!.scheduled_stop_point_in_journey_pattern_ref_id,
+      arrival_time: pt.arrival_time
+        ? Duration.fromISO(pt.arrival_time)
+        : pt.arrival_time,
+      departure_time: pt.departure_time
+        ? Duration.fromISO(pt.departure_time)
+        : pt.departure_time,
+    };
+  });
+
+  return {
+    _passing_times: processedPassingTimes,
+  };
 };
 
 const processBlock = (block: VehicleServiceBlockInput) => {
@@ -145,6 +208,7 @@ const processVehicleScheduleFrame = (
 export const buildTimetablesTableData = (
   input: TimetablesDataset,
 ): TableData<HslTimetablesDbTables>[] => {
+  datasetInput = input;
   const result = cloneDeep(input);
 
   const stuff = processVehicleScheduleFrame(result._vehicle_schedule_frame);
