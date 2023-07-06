7d9c87ee008cc313dc9e21cabd89eafaab2d97e9
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-07-06 11:47:02 +0300

    Add tests for get_vehicle_schedules_on_date function.
    We also add one substitute operating day to additional datasets. In the last part of the tests,
    we modify the one dataset in the test to avoid adding 5-10 different dataset just to change one
    or two attributes.

--- test/hasura/generic/timetablesdb/datasets/types.ts
+++ test/hasura/generic/timetablesdb/datasets/types.ts
@@ -5,6 +5,7 @@ export type UUID = string;
 export enum TimetablePriority {
   Standard = 10, // used for "normal" in-use entities
   Temporary = 20, // overrides Standard, used for temporary adjustments
+  SubstituteOperatingDayByLineType = 23, // overrides standard and temporary
   Special = 25, // special day that overrides Standard and Temporary
   Draft = 30, // overrides Special, Temporary and Standard, not visible to external systems
   Staging = 40, // imported from Hastus, not in use until priority is changed
