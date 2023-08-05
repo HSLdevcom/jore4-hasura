e39d53dd54e998da3e60e3a8521356a1dbbfad14
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-08-05 18:29:03 +0300

    Add CLI for inserting datasets from json files

---
+++ test/hasura/timetables-data-inserter/generic/json-parser.ts
@@ -0,0 +1,35 @@
+import { timetablesDbConfig } from '@config';
+import { closeDbConnection, createDbConnection } from '@util/db';
+import { setupDb } from '@util/setup';
+import { ConnectionConfig } from 'pg';
+import { buildGenericTimetablesDataset } from './dataset';
+import * as test from './example.json';
+import { genericTimetablesJsonSchema } from './json-schemas';
+import { createGenericTableData } from './table-data';
+import { GenericTimetablesDatasetInput } from './types';
+
+export const parseGenericDatasetJson = (
+  input: string,
+): GenericTimetablesDatasetInput => {
+  const parsedJson = JSON.parse(input);
+  const parsedDatasetInput = genericTimetablesJsonSchema.parse(parsedJson);
+  return parsedDatasetInput;
+};
+
+export const insertDatasetFromJson = async (
+  input: string,
+  dbConfig: ConnectionConfig = timetablesDbConfig,
+) => {
+  const result = parseGenericDatasetJson(input);
+  const builtDataset = buildGenericTimetablesDataset(result);
+  const dbConnection = createDbConnection(dbConfig);
+  const tableData = createGenericTableData(builtDataset);
+  await setupDb(dbConnection, tableData);
+  closeDbConnection(dbConnection);
+  return builtDataset;
+};
+
+// TODO: replicate this in a test.
+export const testParsing = async () => {
+  insertDatasetFromJson(JSON.stringify(test));
+};
