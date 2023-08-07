aa3f2e3fc4a4ad0d7bebf67c732d20df5cb528d8
Henrik Hartiala <henrik.hartiala@vincit.fi>
2023-08-07 16:23:01 +0300

    Add comment to clarify expectErrorMessage()

--- test/hasura/util/response.ts
+++ test/hasura/util/response.ts
@@ -1,5 +1,14 @@
 import { Response } from 'request';
 
+/**
+ * NOTE: This currently works differently than expectNoErrorResponse()
+ * as this is a higher order function and the other one is not despite
+ * the same naming. To use this you need to call
+ * expectErrorResponse('ExpectedErrorMessageHere')(response),
+ * TODO: refactor this to be consistent with expectNoErrorResponse(), by
+ * making this a normal function, or by making the function name more
+ * precise. Also the expectedErrorMsg is actually expectedErrorMsgSubstring
+ */
 export const expectErrorResponse =
   (expectedErrorMsg?: string) => (response: Response) => {
     if (response.statusCode >= 200 && response.statusCode < 300)
