dfc8eb7b023f776cb5425316a076cf257f6d5eaf
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-08-21 09:30:29 +0300

    Adjust hostname if using localhost in timetables data inserter
    This is a node problem so makes sense to add a workaround
    here instead on the caller side.
    
    See a5ef543a9bacd93db6a077309f50180a3d40cb34

--- test/hasura/timetables-data-inserter/cli.ts
+++ test/hasura/timetables-data-inserter/cli.ts
@@ -6,11 +6,19 @@ import { ConnectionConfig } from 'pg';
 import { insertDatasetFromJson as insertGenericDatasetFromJson } from './generic/json-parser';
 import { insertDatasetFromJson as insertHslDatasetFromJson } from './hsl/json-parser';
 
+const getHost = (host: string) => {
+  // "localhost" does not currently seem to be working on node 18.
+  if (host === 'localhost') {
+    return '127.0.0.1';
+  }
+  return host;
+};
+
 const buildDbConfig = (
   options: Record<string, string>,
 ): ConnectionConfig | undefined => {
   const dbConfig = {
-    host: options.host,
+    host: getHost(options.host),
     port: options.port ? parseInt(options.port, 10) : undefined,
     database: options.database,
     user: options.user,
