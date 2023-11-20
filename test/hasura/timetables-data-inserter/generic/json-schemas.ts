a5ef038e3a04a890102219fdd83e0bb3e7ccf3f7
Tommi Leinamo <tommi.leinamo@vincit.fi>
2023-11-20 18:54:19 +0200

    Don't allow extra properties in data inserter JSONs
    Unfortunately there doesn't seem to be any "deep strict" mode in Zod,
    so we have to add them individually like this.

--- test/hasura/timetables-data-inserter/generic/json-schemas.ts
+++ test/hasura/timetables-data-inserter/generic/json-schemas.ts
@@ -24,6 +24,7 @@ export const localizedStringSchema = z
     fi_FI: z.string(),
     sv_FI: z.string(),
   })
+  .strict()
   .transform((name) => name as LocalizedString);
 
 // Design choice: pass these enums by key value instead of the final value. Makes reading the easier.
@@ -50,75 +51,94 @@ const defaultDayTypeIdKeys = [
 ] as const;
 
 export type testType = typeof test;
-const stopPointSchema = z.object({
-  scheduled_stop_point_label: z.string(),
-  scheduled_stop_point_sequence: z.number(),
-});
+const stopPointSchema = z
+  .object({
+    scheduled_stop_point_label: z.string(),
+    scheduled_stop_point_sequence: z.number(),
+  })
+  .strict();
 
-const journeyPatternRefSchema = z.object({
-  journey_pattern_ref_id: z.string().uuid().optional(),
-  observation_timestamp: dateTimeSchema.optional(),
-  type_of_line: z.nativeEnum(TypeOfLine).optional(),
-  snapshot_timestamp: dateTimeSchema.optional(),
-  journey_pattern_id: z.string().uuid().optional(),
+const journeyPatternRefSchema = z
+  .object({
+    journey_pattern_ref_id: z.string().uuid().optional(),
+    observation_timestamp: dateTimeSchema.optional(),
+    type_of_line: z.nativeEnum(TypeOfLine).optional(),
+    snapshot_timestamp: dateTimeSchema.optional(),
+    journey_pattern_id: z.string().uuid().optional(),
 
-  _stop_points: z.array(stopPointSchema).optional(),
-});
+    _stop_points: z.array(stopPointSchema).optional(),
+  })
+  .strict();
 
-export const timetabledPassingTimeSchema = z.object({
-  timetabled_passing_time_id: z.string().uuid().optional(),
-  arrival_time: durationSchema.nullable().optional(),
-  departure_time: durationSchema.nullable().optional(),
-  vehicle_journey_id: z.string().uuid().optional(),
-  scheduled_stop_point_in_journey_pattern_ref_id: z.string().uuid().optional(),
-  _scheduled_stop_point_label: z.string(),
-});
+export const timetabledPassingTimeSchema = z
+  .object({
+    timetabled_passing_time_id: z.string().uuid().optional(),
+    arrival_time: durationSchema.nullable().optional(),
+    departure_time: durationSchema.nullable().optional(),
+    vehicle_journey_id: z.string().uuid().optional(),
+    scheduled_stop_point_in_journey_pattern_ref_id: z
+      .string()
+      .uuid()
+      .optional(),
+    _scheduled_stop_point_label: z.string(),
+  })
+  .strict();
 
-export const vehicleJourneySchema = z.object({
-  vehicle_journey_id: z.string().uuid().optional(),
-  block_id: z.string().uuid().optional(),
-  journey_pattern_ref_id: z.string().uuid().optional(),
+export const vehicleJourneySchema = z
+  .object({
+    vehicle_journey_id: z.string().uuid().optional(),
+    block_id: z.string().uuid().optional(),
+    journey_pattern_ref_id: z.string().uuid().optional(),
 
-  _journey_pattern_ref_name: z.string(),
-  _passing_times: z.array(timetabledPassingTimeSchema).optional(),
-});
+    _journey_pattern_ref_name: z.string(),
+    _passing_times: z.array(timetabledPassingTimeSchema).optional(),
+  })
+  .strict();
 
-export const vehicleServiceBlockSchema = z.object({
-  block_id: z.string().uuid().optional(),
-  vehicle_service_id: z.string().uuid().optional(),
+export const vehicleServiceBlockSchema = z
+  .object({
+    block_id: z.string().uuid().optional(),
+    vehicle_service_id: z.string().uuid().optional(),
 
-  _vehicle_journeys: z.record(vehicleJourneySchema).optional(),
-});
+    _vehicle_journeys: z.record(vehicleJourneySchema).optional(),
+  })
+  .strict();
 
-export const vehicleServiceSchema = z.object({
-  vehicle_service_id: z.string().uuid().optional(),
-  day_type_id: z
-    .enum(defaultDayTypeIdKeys)
-    .transform((dayTypeIdKey) => defaultDayTypeIds[dayTypeIdKey])
-    .or(z.string().uuid()),
-  vehicle_schedule_frame_id: z.string().uuid().optional(),
+export const vehicleServiceSchema = z
+  .object({
+    vehicle_service_id: z.string().uuid().optional(),
+    day_type_id: z
+      .enum(defaultDayTypeIdKeys)
+      .transform((dayTypeIdKey) => defaultDayTypeIds[dayTypeIdKey])
+      .or(z.string().uuid()),
+    vehicle_schedule_frame_id: z.string().uuid().optional(),
 
-  _blocks: z.record(vehicleServiceBlockSchema).optional(),
-});
+    _blocks: z.record(vehicleServiceBlockSchema).optional(),
+  })
+  .strict();
 
-export const vehicleScheduleFrameSchema = z.object({
-  vehicle_schedule_frame_id: z.string().uuid().optional(),
-  label: z.string().optional(),
-  // Note: ideally name and name_18n would be mutually exclusive, but there doesn't seem to be a way to implement that with zod.
-  name: z.string().optional(),
-  name_i18n: localizedStringSchema.optional(),
-  validity_start: dateSchema.optional(),
-  validity_end: dateSchema.optional(),
-  priority: z
-    .enum(timetablePriorities)
-    .transform((priority) => TimetablePriority[priority])
-    .optional(),
-  created_at: dateTimeSchema.optional(),
+export const vehicleScheduleFrameSchema = z
+  .object({
+    vehicle_schedule_frame_id: z.string().uuid().optional(),
+    label: z.string().optional(),
+    // Note: ideally name and name_18n would be mutually exclusive, but there doesn't seem to be a way to implement that with zod.
+    name: z.string().optional(),
+    name_i18n: localizedStringSchema.optional(),
+    validity_start: dateSchema.optional(),
+    validity_end: dateSchema.optional(),
+    priority: z
+      .enum(timetablePriorities)
+      .transform((priority) => TimetablePriority[priority])
+      .optional(),
+    created_at: dateTimeSchema.optional(),
 
-  _vehicle_services: z.record(vehicleServiceSchema).optional(),
-});
+    _vehicle_services: z.record(vehicleServiceSchema).optional(),
+  })
+  .strict();
 
-export const genericTimetablesJsonSchema = z.object({
-  _vehicle_schedule_frames: z.record(vehicleScheduleFrameSchema).optional(),
-  _journey_pattern_refs: z.record(journeyPatternRefSchema).optional(),
-});
+export const genericTimetablesJsonSchema = z
+  .object({
+    _vehicle_schedule_frames: z.record(vehicleScheduleFrameSchema).optional(),
+    _journey_pattern_refs: z.record(journeyPatternRefSchema).optional(),
+  })
+  .strict();
