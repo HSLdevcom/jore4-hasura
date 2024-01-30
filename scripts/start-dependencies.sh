67cf974a2a35efd96442a0fca44f35b90b66a9f4
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-01-30 13:51:29 +0200

    Add jore4-tiamat as a remote schema

--- scripts/start-dependencies.sh
+++ scripts/start-dependencies.sh
@@ -12,4 +12,4 @@ echo "Downloading latest version of E2E docker-compose package..."
 curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
 
 # start up test database and hasura for migrations
-docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml up --build "$@" jore4-testdb jore4-hasura jore4-auth
+docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml up --build "$@" jore4-testdb jore4-hasura jore4-auth jore4-tiamat
