f3fb86796a63e954bd28c2b2027ff548b930f4d4
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-03-07 14:16:09 +0200

    Read Tiamat schema location from env
    Read Tiamat schema location from TIAMAT_HOSTNAME and
    TIAMAT_PORT variables, if they are set.
    Use this location for the remote schema metadata

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -19,4 +19,5 @@ REPLACE_PLACEHOLDERS_SCRIPT='/app/scripts/replace-placeholders-in-sql-schema-mig
 HASURA_GRAPHQL_ADMIN_SECRET="$HASURA_ADMIN_SECRET" \
   HASURA_GRAPHQL_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_NAME}" \
   HASURA_TIMETABLES_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_TIMETABLES_NAME}" \
+  HASURA_TIAMAT_SCHEMA_URL="http://${TIAMAT_HOSTNAME:-jore4-tiamat}:${TIAMAT_PORT:-1888}/services/stop_places/graphql" \
   exec /bin/docker-entrypoint.sh "$@"
