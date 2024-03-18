73ec408dbe8f5f8b6879ca8252a7637cd469267e
Teemu Mäkinen <teemu.makinen@vincit.fi>
2024-03-18 14:33:01 +0200

    Directly read from the Tiamat database
    - Use the default database also as the metadata database
    - Create a direct connection to the Tiamat database
    - Track all public tables
    - Track all relevant parent -> child connections

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -16,9 +16,12 @@ REPLACE_PLACEHOLDERS_SCRIPT='/app/scripts/replace-placeholders-in-sql-schema-mig
 /app/scripts/repeatable-migrations.sh
 
 # Pass on the execution to Hasura's own docker-entrypoint.sh file to (re)run the migrations
+# Tiamat database is only needed in HSL schema, if it is not defined it should not stop the container from running
 HASURA_GRAPHQL_ADMIN_SECRET="$HASURA_ADMIN_SECRET" \
+  HASURA_GRAPHQL_METADATA_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_NAME}" \
   HASURA_GRAPHQL_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_NAME}" \
   HASURA_TIMETABLES_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_TIMETABLES_NAME}" \
+  HASURA_TIAMAT_DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:5432/${DB_TIAMAT_NAME:-undefined}" \
   HASURA_TIAMAT_SCHEMA_URL="http://${TIAMAT_HOSTNAME:-jore4-tiamat}:${TIAMAT_PORT:-1888}/services/stop_places/graphql" \
   HASURA_TIMETABLESAPI_URL="http://${TIMETABLESAPI_HOSTNAME:-jore4-timetablesapi}:${TIMETABLESAPI_PORT:-8080}" \
   exec /bin/docker-entrypoint.sh "$@"
