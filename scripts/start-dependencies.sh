f63f1279d0a473c514b66445ec74fb869e8ebf6c
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-01-13 15:11:02 +0200

    From now on, download the JORE4 Docker Compose bundle from the "jore4-docker-compose-bundle" repository.

--- scripts/start-dependencies.sh
+++ scripts/start-dependencies.sh
@@ -1,15 +1,83 @@
 #!/usr/bin/env bash
 
-# based on https://github.com/HSLdevcom/jore4-tools#download-docker-bundlesh
-
 set -euo pipefail
 
 # allow running from any working directory
 WD=$(dirname "$0")
 cd "${WD}/.."
 
-echo "Downloading latest version of E2E docker-compose package..."
-curl https://raw.githubusercontent.com/HSLdevcom/jore4-tools/main/docker/download-docker-bundle.sh | bash
+# Download Docker Compose bundle from the "jore4-docker-compose-bundle"
+# repository. GitHub CLI is required to be installed.
+#
+# A commit reference can be given as an argument. It can contain, for example,
+# only a substring of an actual SHA digest.
+download_docker_compose_bundle() {
+  local commit_ref="${1:-main}"
+
+  local repo_name="jore4-docker-compose-bundle"
+  local repo_owner="HSLdevcom"
+
+  # Check GitHub CLI availability.
+  if ! command -v gh &> /dev/null; then
+    echo "Please install the GitHub CLI (gh) on your machine."
+    exit 1
+  fi
+
+  # Make sure the user is authenticated to GitHub.
+  gh auth status || gh auth login
+
+  echo "Using the commit reference '${commit_ref}' to fetch a Docker Compose bundle..."
+
+  # First, try to find a commit on GitHub that matches the given reference.
+  # This function exits with an error code if no matching commit is found.
+  local commit_sha
+  commit_sha=$(
+    gh api \
+      -H "Accept: application/vnd.github+json" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      "repos/${repo_owner}/${repo_name}/commits/${commit_ref}" \
+      --jq '.sha'
+  )
+
+  echo "Commit with the following SHA digest was found: ${commit_sha}"
+
+  local zip_file="/tmp/${repo_name}.zip"
+  local unzip_target_dir_prefix="/tmp/${repo_owner}-${repo_name}"
+
+  # Remove old temporary directories if any remain.
+  rm -fr "$unzip_target_dir_prefix"-*
+
+  echo "Downloading the JORE4 Docker Compose bundle..."
+
+  # Download the latest Docker Compose bundle from the
+  # jore4-docker-compose-bundle repository as a ZIP file and extract its
+  # contents to a temporary directory.
+  gh api "repos/${repo_owner}/${repo_name}/zipball/${commit_sha}" > "$zip_file" \
+    && unzip -q "$zip_file" -d /tmp
+
+  # Clean untracked files from `docker` directory even if they are git-ignored.
+  git clean -fx ./docker
+
+  # Copy files from the `docker-compose` directory of the ZIP file to your
+  # local `docker` directory.
+  mv "$unzip_target_dir_prefix"-*/docker-compose/* ./docker
+
+  # Remove the temporary files and directories created above.
+  rm -fr "$zip_file" "$unzip_target_dir_prefix"-*
+
+  echo "Generating a release version file for the downloaded bundle..."
+
+  # Create a release version file containing the SHA digest of the referenced
+  # commit.
+  echo "$commit_sha" > ./docker/RELEASE_VERSION.txt
+}
+
+# By default, the tip of the main branch of the jore4-docker-compose-bundle
+# repository is used as the commit reference, which specifies the Docker Compose
+# bundle version to fetch. For debugging purposes, this default can be
+# overridden by passing some commit SHA (or its initial substring) to the
+# download function.
+download_docker_compose_bundle
 
 # start up test database and hasura for migrations
 docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.custom.yml up --build "$@" jore4-testdb jore4-hasura jore4-auth jore4-tiamat
