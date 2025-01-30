d87b10b936a6fb57c5464174e6b6cf996b4db9c3
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-01-30 10:24:48 +0200

    Use more portable shebang in bash shell scripts.
    Add common set command to the beginning of bash shell scripts.
    
    Add missing double quotes to prevent globbing and word splitting.

--- scripts/use-specific-schema.sh
+++ scripts/use-specific-schema.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 if [[ "$PWD" != *scripts ]]; then
   cd "$(dirname "$0")"
 fi
 
-if [[ -z ${1} ]]; then
-  echo "Usage: $(basename $0) environment"
+if [[ $# -eq 0 ]]; then
+  echo "Usage: $(basename "$0") environment"
   echo "Possible environment values are currently:"
   ls -1 -d ../migrations/*/ | cut -f3 -d'/'
   exit 1
 fi
-
-set -euo pipefail
 
 sed -i "s/metadata_directory:.*/metadata_directory: metadata\/${1}/i" ../config.yaml
 sed -i "s/migrations_directory:.*/migrations_directory: migrations\/${1}/i" ../config.yaml
