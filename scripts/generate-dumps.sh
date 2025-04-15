dfb87c738a6ee105faf3391ed3abfbc102554b54
Teemu Mäkinen <teemu.makinen@vincit.fi>
2025-04-15 13:56:02 +0300

    Fix generate-dumps.sh after update of testdb image

--- scripts/generate-dumps.sh
+++ scripts/generate-dumps.sh
@@ -11,7 +11,8 @@ docker exec testdb pg_dump -h localhost -p 5432 -U dbadmin -d jore4e2e --schema-
 docker exec testdb pg_dump -h localhost -p 5432 -U dbadmin -d timetablesdb --schema-only -f /tmp/timetablesdb-dump.sql
 
 # add sorting tool to the container
-docker exec testdb apt-get install python3-minimal python3-docopt -y
+docker exec testdb apt-get update
+docker exec testdb apt-get install python3-minimal python3-docopt curl -y
 docker exec testdb curl -o /tmp/pgdump-sort.py https://raw.githubusercontent.com/tigra564/pgdump-sort/master/pgdump-sort
 
 # sort the dumps
