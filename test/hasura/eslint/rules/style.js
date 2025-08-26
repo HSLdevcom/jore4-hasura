454165c8555c5db133e1ae678a7539f7a6628625
Jesse Jaara <jesse.jaara@vincit.fi>
2025-08-26 11:29:38 +0300

    TDI: ESLint more style cleanups.
    * Drop rules turned 'off', by the Airbnb config, but which were not
      enabled by anything else → Noop rules.
    * Drop config options duplication all or some of the default values.
    * Instead of migrating a deprecated rule about empty lines between
      certain statements, 'lines-around-directive' rule has been
      completely dropped. We are not using any directives, nor is our
      codebase ever expected contain any.
    * Migrate deprecated 'no-new-object' rule to use the new
      'no-object-constructor' rule instead.

--- test/hasura/eslint/rules/style.js
+++ test/hasura/eslint/rules/style.js
@@ -5,167 +5,23 @@ module.exports = {
     // require camel case names
     camelcase: ['error', { properties: 'never', ignoreDestructuring: false }],
 
-    // enforce or disallow capitalization of the first letter of a comment
-    // https://eslint.org/docs/rules/capitalized-comments
-    'capitalized-comments': [
-      'off',
-      'never',
-      {
-        line: {
-          ignorePattern: '.*',
-          ignoreInlineComments: true,
-          ignoreConsecutiveComments: true,
-        },
-        block: {
-          ignorePattern: '.*',
-          ignoreInlineComments: true,
-          ignoreConsecutiveComments: true,
-        },
-      },
-    ],
-
-    // enforces consistent naming when capturing the current execution context
-    'consistent-this': 'off',
-
     // enforce newline at the end of file, with no multiple empty lines
     'eol-last': ['error', 'always'],
-
-    // requires function names to match the name of the variable or property to which they are
-    // assigned
-    // https://eslint.org/docs/rules/func-name-matching
-    'func-name-matching': [
-      'off',
-      'always',
-      {
-        includeCommonJSModuleExports: false,
-        considerPropertyDescriptor: true,
-      },
-    ],
 
     // require function expressions to have a name
     // https://eslint.org/docs/rules/func-names
     'func-names': 'warn',
 
-    // enforces use of function declarations or expressions
-    // https://eslint.org/docs/rules/func-style
-    // TODO: enable
-    'func-style': ['off', 'expression'],
-
-    // disallow specified identifiers
-    // https://eslint.org/docs/rules/id-denylist
-    'id-denylist': 'off',
-
-    // this option enforces minimum and maximum identifier lengths
-    // (variable names, property names etc.)
-    'id-length': 'off',
-
-    // require identifiers to match the provided regular expression
-    'id-match': 'off',
-
-    // enforce position of line comments
-    // https://eslint.org/docs/rules/line-comment-position
-    // TODO: enable?
-    'line-comment-position': [
-      'off',
-      {
-        position: 'above',
-        ignorePattern: '',
-        applyDefaultPatterns: true,
-      },
-    ],
-
     // require or disallow an empty line between class members
     // https://eslint.org/docs/rules/lines-between-class-members
-    'lines-between-class-members': [
-      'error',
-      'always',
-      { exceptAfterSingleLine: false },
-    ],
-
-    // require or disallow newlines around directives
-    // https://eslint.org/docs/rules/lines-around-directive
-    'lines-around-directive': [
-      'error',
-      {
-        before: 'always',
-        after: 'always',
-      },
-    ],
-
-    // Require or disallow logical assignment logical operator shorthand
-    // https://eslint.org/docs/latest/rules/logical-assignment-operators
-    // TODO, semver-major: enable
-    'logical-assignment-operators': [
-      'off',
-      'always',
-      {
-        enforceForIfStatements: true,
-      },
-    ],
-
-    // specify the maximum depth that blocks can be nested
-    'max-depth': ['off', 4],
-
-    // specify the max number of lines in a file
-    // https://eslint.org/docs/rules/max-lines
-    'max-lines': [
-      'off',
-      {
-        max: 300,
-        skipBlankLines: true,
-        skipComments: true,
-      },
-    ],
-
-    // enforce a maximum function length
-    // https://eslint.org/docs/rules/max-lines-per-function
-    'max-lines-per-function': [
-      'off',
-      {
-        max: 50,
-        skipBlankLines: true,
-        skipComments: true,
-        IIFEs: true,
-      },
-    ],
-
-    // specify the maximum depth callbacks can be nested
-    'max-nested-callbacks': 'off',
-
-    // limits the number of parameters that can be used in the function declaration.
-    'max-params': ['off', 3],
-
-    // specify the maximum number of statement allowed in a function
-    'max-statements': ['off', 10],
-
-    // enforce a particular style for multiline comments
-    // https://eslint.org/docs/rules/multiline-comment-style
-    'multiline-comment-style': ['off', 'starred-block'],
+    'lines-between-class-members': ['error', 'always'],
 
     // require a capital letter for constructors
-    'new-cap': [
-      'error',
-      {
-        newIsCap: true,
-        newIsCapExceptions: [],
-        capIsNew: false,
-        capIsNewExceptions: [
-          'Immutable.Map',
-          'Immutable.Set',
-          'Immutable.List',
-        ],
-      },
-    ],
+    'new-cap': ['error', { capIsNew: false }],
 
     // disallow the omission of parentheses when invoking a constructor with no arguments
     // https://eslint.org/docs/rules/new-parens
     'new-parens': 'error',
-
-    // allow/disallow an empty newline after var statement
-    'newline-after-var': 'off',
-
-    // https://eslint.org/docs/rules/newline-before-return
-    'newline-before-return': 'off',
 
     // disallow use of bitwise operators
     // https://eslint.org/docs/rules/no-bitwise
@@ -174,9 +30,6 @@ module.exports = {
     // disallow use of the continue statement
     // https://eslint.org/docs/rules/no-continue
     'no-continue': 'error',
-
-    // disallow comments inline after code
-    'no-inline-comments': 'off',
 
     // disallow if as the only statement in an else block
     // https://eslint.org/docs/rules/no-lonely-if
@@ -190,15 +43,11 @@ module.exports = {
     // https://eslint.org/docs/rules/no-multiple-empty-lines
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
 
-    // disallow negated conditions
-    // https://eslint.org/docs/rules/no-negated-condition
-    'no-negated-condition': 'off',
-
     // disallow nested ternary expressions
     'no-nested-ternary': 'error',
 
     // disallow use of the Object constructor
-    'no-new-object': 'error',
+    'no-object-constructor': 'error',
 
     // disallow certain syntax forms
     // https://eslint.org/docs/rules/no-restricted-syntax
@@ -226,20 +75,9 @@ module.exports = {
       },
     ],
 
-    // disallow the use of ternary operators
-    'no-ternary': 'off',
-
     // disallow dangling underscores in identifiers
     // https://eslint.org/docs/rules/no-underscore-dangle
-    'no-underscore-dangle': [
-      'error',
-      {
-        allow: [],
-        allowAfterThis: false,
-        allowAfterSuper: false,
-        enforceInMethodNames: true,
-      },
-    ],
+    'no-underscore-dangle': ['error', { enforceInMethodNames: true }],
 
     // disallow the use of Boolean literals in conditional expressions
     // also, prefer `a || b` over `a ? a : b`
@@ -257,10 +95,6 @@ module.exports = {
     // https://eslint.org/docs/rules/operator-assignment
     'operator-assignment': ['error', 'always'],
 
-    // Require or disallow padding lines between statements
-    // https://eslint.org/docs/rules/padding-line-between-statements
-    'padding-line-between-statements': 'off',
-
     // Disallow the use of Math.pow in favor of the ** operator
     // https://eslint.org/docs/rules/prefer-exponentiation-operator
     'prefer-exponentiation-operator': 'error',
@@ -268,12 +102,6 @@ module.exports = {
     // Prefer use of an object spread over Object.assign
     // https://eslint.org/docs/rules/prefer-object-spread
     'prefer-object-spread': 'error',
-
-    // requires object keys to be sorted
-    'sort-keys': ['off', 'asc', { caseSensitive: false, natural: true }],
-
-    // sort variables within the same declaration block
-    'sort-vars': 'off',
 
     // require or disallow a space immediately following the // or /* in a comment
     // https://eslint.org/docs/rules/spaced-comment
