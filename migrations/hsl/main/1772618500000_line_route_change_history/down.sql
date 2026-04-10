3d4a953eaab84078aabeeaa9c654c499b39b0f96
Teemu Mäkinen <teemu.makinen@vincit.fi>
2026-04-10 13:31:12 +0300

    Add line and route history to main database

---
+++ migrations/hsl/main/1772618500000_line_route_change_history/down.sql
@@ -0,0 +1,34 @@
+DROP TRIGGER IF EXISTS record_history_after_route_journey_pattern_stop_points_inserted
+  ON network.scheduled_stop_point_in_journey_pattern;
+DROP TRIGGER IF EXISTS record_history_after_route_journey_pattern_stop_points_updated
+  ON network.scheduled_stop_point_in_journey_pattern;
+DROP TRIGGER IF EXISTS record_history_after_route_journey_pattern_stop_points_deleted
+  ON network.scheduled_stop_point_in_journey_pattern;
+DROP FUNCTION IF EXISTS network.record_history_after_route_journey_pattern_stop_points_changed();
+
+DROP TRIGGER IF EXISTS record_history_after_route_infra_links_inserted
+  ON network.infrastructure_link_along_route;
+DROP TRIGGER IF EXISTS record_history_after_route_infra_links_updated
+  ON network.infrastructure_link_along_route;
+DROP TRIGGER IF EXISTS record_history_after_route_infra_links_deleted
+  ON network.infrastructure_link_along_route;
+DROP FUNCTION IF EXISTS network.record_history_after_route_infra_links_changed();
+
+DROP TRIGGER IF EXISTS record_history_before_route_deleted ON network.route;
+DROP FUNCTION IF EXISTS network.record_history_before_route_deleted();
+
+DROP TRIGGER IF EXISTS record_history_after_route_upserted ON network.route;
+DROP FUNCTION IF EXISTS network.record_history_after_route_upserted();
+
+DROP TRIGGER IF EXISTS record_history_before_line_deleted ON network.line;
+DROP FUNCTION IF EXISTS network.record_history_before_line_deleted();
+
+DROP TRIGGER IF EXISTS record_history_after_line_upserted ON network.line;
+DROP FUNCTION IF EXISTS network.record_history_after_line_upserted();
+
+DROP FUNCTION IF EXISTS network.record_history_for_changed_route(tgOperation TEXT, routeId UUID);
+DROP FUNCTION IF EXISTS network.record_history_for_changed_line(tgOperation TEXT, lineId UUID);
+DROP FUNCTION IF EXISTS network.get_higher_priority_operation(previousOperation TEXT, nextOperation TEXT);
+DROP FUNCTION IF EXISTS network.collect_line_info_for_history(UUID);
+
+DROP TABLE IF EXISTS network.line_change_history;
