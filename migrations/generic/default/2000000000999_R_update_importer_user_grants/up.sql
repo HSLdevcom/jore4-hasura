d1c514eaca6234798091025f9906e7aa7b4cd5cc
Janne Bergman <janne.bergman@vincit.fi>
2026-09-21 09:27:14 +0300

    Better logging on journey-pattern errors

---
+++ migrations/generic/default/2000000000999_R_update_importer_user_grants/up.sql
@@ -0,0 +1,24 @@
+GRANT INSERT ON ALL TABLES IN SCHEMA service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT UPDATE ON ALL TABLES IN SCHEMA service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT DELETE ON ALL TABLES IN SCHEMA service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT TRUNCATE ON ALL TABLES IN SCHEMA service_pattern TO xxx_db_jore3importer_username_xxx;
+
+GRANT INSERT ON ALL TABLES IN SCHEMA journey_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT UPDATE ON ALL TABLES IN SCHEMA journey_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT DELETE ON ALL TABLES IN SCHEMA journey_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT TRUNCATE ON ALL TABLES IN SCHEMA journey_pattern TO xxx_db_jore3importer_username_xxx;
+
+GRANT INSERT ON ALL TABLES IN SCHEMA internal_service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT UPDATE ON ALL TABLES IN SCHEMA internal_service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT DELETE ON ALL TABLES IN SCHEMA internal_service_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT TRUNCATE ON ALL TABLES IN SCHEMA internal_service_pattern TO xxx_db_jore3importer_username_xxx;
+
+GRANT INSERT ON ALL TABLES IN SCHEMA route TO xxx_db_jore3importer_username_xxx;
+GRANT UPDATE ON ALL TABLES IN SCHEMA route TO xxx_db_jore3importer_username_xxx;
+GRANT DELETE ON ALL TABLES IN SCHEMA route TO xxx_db_jore3importer_username_xxx;
+GRANT TRUNCATE ON ALL TABLES IN SCHEMA route TO xxx_db_jore3importer_username_xxx;
+
+GRANT INSERT ON ALL TABLES IN SCHEMA timing_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT UPDATE ON ALL TABLES IN SCHEMA timing_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT DELETE ON ALL TABLES IN SCHEMA timing_pattern TO xxx_db_jore3importer_username_xxx;
+GRANT TRUNCATE ON ALL TABLES IN SCHEMA timing_pattern TO xxx_db_jore3importer_username_xxx;
